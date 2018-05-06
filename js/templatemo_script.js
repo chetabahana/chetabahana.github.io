6790568418be54a4ff3c7baa6c608eb0e76f562b
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-05-06 20:18:30 +0700

    Update templatemo_script.js

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -15,8 +15,7 @@ jQuery(function($) {
         document.body.appendChild(script);*/
 	    
         initialize($('#chetabahana-skema'));
-        //$( "tspan:contains('Buka Toko')" ).css( "cursor","pointer" );
-	    $( "tspan:contains('Buka Toko')" ).css( "text-decoration", "underline" );
+        $( "g:contains('Buka Toko')" ).css( "cursor","pointer" );
 	    
         top_menu_height = $('.templatemo-top-menu').height();
         // scroll spy to auto active the nav item
