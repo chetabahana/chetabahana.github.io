cc4ec2eb7d10ed7327f84d17f3e16bac481808e4
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-05-23 16:59:53 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -47,4 +47,16 @@ function updateFilters() {
       //console.log(pdiv);
     }
   };
-}    
+}  
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
