8424718e2680cee26f58e6b6390198fabe0d560a
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-06-05 02:23:31 +0700

    Update templatemo_script.js

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -10,8 +10,7 @@ jQuery(function($) {
         $('.templatemo-top-menu').stickUp(); 
 	    
         // clear the hash anytime someone arrives with a hash tag    
-        if( typeof(location.hash) !== 'undefined' && location.hash.length ) {
-            e.preventDefault();console.log(location.hash); 
+        if( typeof(location.hash) !== 'undefined' && location.hash.length ) {console.log(location.hash); 
             scrollTo(location.hash); location.hash = '';
             //location.href = location.href.substr(0, location.href.indexOf('#'));						     
             return false;
