61864414b9a3c4821669b39709320bf740ad27c7
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-06-09 21:06:33 +0700

    Update chetabahana-skema.js

--- js/chetabahana-skema.js
+++ js/chetabahana-skema.js
@@ -39,20 +39,36 @@ var draw = {
          } else if(type == 'flowchart'){     
       
            $.getJSON(jsonfile, options).done(function(result){
-              $('.contact_left').hide(); 
+           
+              var a = Number((draw.point.substr(0,1)));
+              var b = Number((draw.point.substr(1,1)));
+              
               var obj = result.items[3];
+              //var obj = result.items[a].items[b];
               var code = draw.encode(obj.content_html);
+              
+              $('.contact_left').hide(); 
               diagram = flowchart.parse(code);      
               diagram.drawSVG($('.diagram').get(0), obj.options);
+              
             });               
         
          } else if(type == 'railroad'){
          
             $.getJSON(jsonfile, options).done(function(result){
+            
+              var a = Number((draw.point.substr(0,1)));
+              var b = Number((draw.point.substr(1,1)));
+              var c = Number((draw.point.substr(2,1)));
+              var d = Number((draw.point.substr(3,2)));
+              
               var obj = result.items[2];
-              var code = draw.encode(obj.content_html);             
+             //var obj = result.items[a].items[b].items[c].items[d];
+              var code = draw.encode(obj.content_html);
+              
               diagram = eval(code).format();
               diagram.addTo($('.diagram').get(0));
+              
             });               
         
          }
@@ -75,32 +91,91 @@ var draw = {
     
       switch(draw.type) {
       
-        case 'flowchart': 
+        case 'sequence':
+        
+          $('svg g.actor').each(function( index ) {
+             this.id = '0' + Math.floor(index/2).toString();
+          });
+          $('svg>path').each(function( index ) {
+             this.id = '1' + index.toString();
+          });
+          $('svg g.signal').each(function( index ) {
+             this.id = '2' + index.toString();
+          });
+          draw.elements = $('svg g.actor, svg g.signal, svg>path');
+          
+        break;
+          
+        case 'flowchart':
+        
+          $('svg .flowchart').each(function( index ) {
+             this.id = draw.point + draw.pad('0' + index, 3);
+          });
+          $('svg text').each(function( index ) {
+             this.id = draw.point + draw.pad('1' + index, 3);
+          });
           draw.elements = $('svg .flowchart, svg text');
-          break;
+          
+        break;
           
         case 'railroad':
+        
+          $('svg rect').each(function( index ) {
+             this.id = draw.point + draw.pad('0' + index, 3);
+          });
+          $('svg text').each(function( index ) {
+             this.id = draw.point + draw.pad('1' + index, 4);
+          });
           draw.elements = $('svg rect, svg text');
-          break;
           
-        default:
-          draw.elements = $('svg g, svg path');
+        break;
+          
       }      
-      
-      draw.elements.css('cursor','pointer')
-        .hover(function() {
-          $(this).hide(100).show(100);
-      }).click(function() {
-          draw.type = (draw.type == 'sequence')? 
-            'flowchart': 'railroad';
-          draw.point = $(this);
-          draw.diagram();
-      });  
       
       $('#loadingImg').hide();
       
+      draw.elements.css('cursor','pointer')
+        .hover(function() {
+        
+          $(this).hide(100).show(100);
+          
+      }).click(function() {
+      
+          if (draw.type != 'railroad') {
+          
+            draw.type = (draw.type == 'sequence')? 
+              'flowchart': 'railroad';
+            draw.point = this.id;
+            draw.diagram();
+          
+          } else {
+          
+            var a = Number((this.id.substr(0,1)));
+            var b = Number((this.id.substr(1,1)));
+            var c = Number((this.id.substr(2,1)));
+            var d = Number((this.id.substr(3,2)));          
+            var e = Number((this.id.substr(5,1)));
+            var f = Number((this.id.substr(6,3)));
+            
+            console.log(f);
+          
+          }
+          
+      });  
+      
     } 
   },
+
+  xmlData : function() {
+    var a = $(this);
+    var svg = $(".diagram").find('svg')[0];
+    var width = parseInt(svg.width.baseVal.value);
+    var height = parseInt(svg.height.baseVal.value);
+    var xmldata = '<?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd"><svg xmlns="http://www.w3.org/2000/svg" width="' + width + '" height="' + height + '" xmlns:xlink="http://www.w3.org/1999/xlink"><source><![CDATA[' + draw.skema + ']]></source>' + svg.innerHTML + '</svg>';
+    a.attr("download", "diagram.svg"); 
+    var xml = encodeURIComponent(xmldata);
+    a.attr("href", "data:image/svg+xml," + xml);
+  },  
    
   encode : function(data) {  
     return data.replace(/&apos;/g, "'")
@@ -115,14 +190,9 @@ var draw = {
     ;
   },               
 
-  xmlData : function() {
-    var a = $(this);
-    var svg = $(".diagram").find('svg')[0];
-    var width = parseInt(svg.width.baseVal.value);
-    var height = parseInt(svg.height.baseVal.value);
-    var xmldata = '<?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd"><svg xmlns="http://www.w3.org/2000/svg" width="' + width + '" height="' + height + '" xmlns:xlink="http://www.w3.org/1999/xlink"><source><![CDATA[' + draw.skema + ']]></source>' + svg.innerHTML + '</svg>';
-    a.attr("download", "diagram.svg"); 
-    var xml = encodeURIComponent(xmldata);
-    a.attr("href", "data:image/svg+xml," + xml);
-  },  
+  pad : function(data, size) {
+    var s = String(data);
+    while (s.length < (size || 2)) {s = "0" + s;}
+    return s;
+  },
 }
