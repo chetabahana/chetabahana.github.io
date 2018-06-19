f2d0498503e2776b8ed145854e907127558e61d3
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-06-20 01:47:25 +0700

    Update method.js

--- _layouts/skema/sequence/method.js
+++ _layouts/skema/sequence/method.js
@@ -1,1 +1,1 @@
-{{ page.path | split: '/' | shift  | shift }}{{ page.from }}{{ page.type }}{{ page.to }}: {{ page.label }}
+{{ page.path | split: '/' | shift | shift }}{{ page.from }}{{ page.type }}{{ page.to }}: {{ page.label }}
