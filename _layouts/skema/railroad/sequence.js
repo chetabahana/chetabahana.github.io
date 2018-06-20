a3598dc73c674875ebb44217a56f43ff5311e62b
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-06-21 00:32:26 +0700

    Update sequence.js

--- _layouts/skema/railroad/sequence.js
+++ _layouts/skema/railroad/sequence.js
@@ -1,1 +1,2 @@
-{% assign object_items = site.assets | where_exp:"item", "item.ext == '.coffee'" %}{% for item in object_items %}{{ item.title }}{% endfor %}
+{% assign object_items = site.assets | where_exp:"item", "item.ext == '.coffee'" %}
+{% for item in object_items %}{{ item.ext}}{% endfor %}
