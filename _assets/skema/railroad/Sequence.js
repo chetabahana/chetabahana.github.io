f7f868d89e60de663c93b81e958553e15d3b3236
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-06-22 04:21:38 +0700

    Update Sequence.js

--- _assets/skema/railroad/Sequence.js
+++ _assets/skema/railroad/Sequence.js
@@ -2,4 +2,4 @@
 weight: 1
 layout: null
 ---
-{% assign my_page_layout = page.path | split: "/" | shift | pop | join: "/" | append: "/" | append: page.slug | downcase %}{% assign my_page_path = my_page_layout | append: "/" %}{% assign my_childs = site.assets | where_exp:"item", "item.layout == my_page_layout" | where_exp:"item", "item.path contains my_page_path" %}{{ my_page_layout }}{{ my_childs | inspect }}{{ my_page_path }}
+{% assign my_page_layout = page.path | split: "/" | shift | pop | join: "/" | append: "/" | append: page.slug | downcase %}{% assign my_page_path = my_page_layout | append: "/" %}{% assign my_childs = site.assets | where_exp:"item", "item.layout == my_page_layout" | where_exp:"item", "item.path contains my_page_path" %}{% if my_childs.size > 0 %}{% capture my_childs_content %}{% for my_child in my_childs %}{{ my_child.content }}{% endfor %}{% endcapture %}{{ my_childs_content | jsonify }}{% endif %}
