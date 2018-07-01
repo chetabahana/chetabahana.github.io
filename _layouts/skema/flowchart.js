9942190ab7c2da960b5440f6daa66bd8aa032cfd
Chetabahana <36441664+chetabahana@users.noreply.github.com>
2018-07-01 15:15:50 +0700

    Update flowchart.js

--- _layouts/skema/flowchart.js
+++ _layouts/skema/flowchart.js
@@ -2,16 +2,16 @@ st=>start: Start|past
 e=>end: End|past
 op1=>operation: Input|future
 op2=>operation: Output|future
-io1=>inputoutput: Fix Input|current
-io2=>inputoutput: Fix Output|current
+sub1=>subroutine: Sub Input|current
+sub2=>subroutine: Sub Output|current
 uji1=>condition: Uji Input|rejected
 uji2=>condition: Uji Output|approved
-sub1=>subroutine: Sub Input|invalid
-sub2=>subroutine: Sub Output|invalid
+io1=>inputoutput: Fix Input|invalid
+io2=>inputoutput: Fix Output|invalid
 
-st->op1(right)->io1
-io1(right)->uji1(yes, right)->io2
-uji1(no)->sub1(left)->io1
-io2->uji2(yes, right)->op2
-uji2(no)->sub2(left)->sub1
+st->op1(right)->sub1
+sub1(right)->uji1(yes, right)->sub2
+uji1(no)->io1(left)->sub1
+sub2->uji2(yes, right)->op2
+uji2(no)->io2(left)->io1
 op2->e
