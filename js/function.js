42f5defb040a468fcf85016beb8520ba34702146
Chetabahana <chetabahana@gmail.com>
2019-09-23 22:51:44 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,11 +1,6 @@
 /**
 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-
-var disqus_config = function () {
-this.page.url = 'https://chetabahana.github.io/';  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = ''; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
 fetch('feed.json')
   .then(response => {
     return response.json()
@@ -17,6 +12,12 @@ fetch('feed.json')
   .catch(err => {
     // Do something for an error here
   }) 
+
+var disqus_config = function () {
+this.page.url = 'https://chetabahana.github.io/';  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = ''; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
 s.src = 'https://chetabahana.disqus.com/embed.js';
