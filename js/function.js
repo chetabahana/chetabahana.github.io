ffb9f70e6cb333c12e8bde264cf76b9f2a358116
Chetabahana <chetabahana@gmail.com>
2019-09-24 10:26:44 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -2,7 +2,8 @@
 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
 
-  fetch('https://chetabahana.github.io/0/1/2/3/4/5/6/7/8/9/assets/feed.json?t=123', {
+function addUser(details="") {
+  return fetch('https://api.example.com/user', {
     mode: 'cors',
     method: 'POST',
     credentials: 'include',
@@ -15,12 +16,13 @@
   }).then(response => {
     return response.json().then(data => {
       if (response.ok) {
-        console.log(data);
+        return data;
       } else {
-        console.log(Promise.reject({status: response.status, data}));
+        return Promise.reject({status: response.status, data});
       }
     });
   });
+}
 
 var disqus_config = function () {
 this.page.url = 'https://chetabahana.github.io/';  // Replace PAGE_URL with your page's canonical URL variable
