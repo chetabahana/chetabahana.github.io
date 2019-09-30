1dd84d74bb33eebc0b178f937fe5f1372ee5b445
Chetabahana <chetabahana@gmail.com>
2019-09-30 23:05:14 +0700

    Update chetabahana-skema.js

--- js/chetabahana-skema.js
+++ js/chetabahana-skema.js
@@ -174,7 +174,7 @@ var draw = {
       }
 
       $('#loadingImg').hide();
-      $('.editor-wrapper').height($('.diagram').height());
+      $('.editor').height($('.diagram').height());
 
       draw.elements.css({'cursor':'pointer'})
 
