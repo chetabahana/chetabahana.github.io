94f5a3c1209b920d6dff3be5af312c6df501e9f7
Chetabahana <chetabahana@gmail.com>
2019-10-02 14:15:33 +0700

    Update chetabahana-skema.js

--- js/chetabahana-skema.js
+++ js/chetabahana-skema.js
@@ -5,7 +5,7 @@ $('.download').click(function(ev) {draw.xmlData();});
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/crimson_editor");
 editor.getSession().setMode("ace/mode/asciidoc");
-//editor.getSession().on('change', _.debounce(function() {draw.diagram();}, 100) );
+editor.getSession().on('change', _.debounce(function() {draw.diagram();}, 100) );
 
 var draw = {
 
@@ -20,15 +20,11 @@ var draw = {
         var select = $(".theme").val();
         
         var type = (!draw.type)? 'sequence': draw.type;
-        if (select=='hand' && type!='flowchart') type='sequence';
-
         var js = '/' + this[type] + '?t=' + $.now();
-        var jsonfile = '/' + this['jsonfeed'] + '?t=' + $.now();
-        if (draw.point) jsonfile = jsonfile.replace('assets', draw.point);
+        if (select=='hand' && type!='flowchart') type='sequence';
 
         $('#type').text(type);
         $('#type')[0].href = '/' + type;
-        $("#json").attr("href", jsonfile)
         $('.diagram').html(''); $("#loadingImg").show();
 
         $.getScript(js, function( data, textStatus, jqxhr ) {
@@ -47,53 +43,21 @@ var draw = {
 
                     } else {
 
-                        $.getJSON(jsonfile).done(function(result){
+                        diagram = Diagram.parse(draw.skema);
+                        draw.opt = {theme: select, "font-size": 13};
+                        diagram.drawSVG($('.diagram').get(0), draw.opt);
 
-                            var obj = result.items[4].items[0];
-                            draw.skema = draw.encode(obj.query);
-
-                            editor.setValue(draw.skema);
-                            diagram = Diagram.parse(draw.skema);
-                            draw.opt = {theme: select, "font-size": 13};
-                            diagram.drawSVG($('.diagram').get(0), draw.opt);
-
-                        });
-     
                     }
 
                 } else if(type == 'flowchart'){
 
-                    $.getJSON(jsonfile, draw.opt).done(function(result){
-
-                        var a = Number((draw.point.substr(0,1)));
-                        var b = Number((draw.point.substr(1,1)));
-
-                        var obj = result.items[4].items[1];
-                        draw.skema = draw.encode(obj.query);
-
-                        editor.setValue(draw.skema);
-                        diagram = flowchart.parse(draw.skema);
-                        diagram.drawSVG($('.diagram').get(0), obj.input);
-
-                    });
+                    diagram = flowchart.parse(draw.skema);
+                    diagram.drawSVG($('.diagram').get(0), obj.input);
 
                 } else if(type == 'railroad'){
 
-                    $.getJSON(jsonfile, draw.opt).done(function(result){
-
-                        var a = Number((draw.point.substr(0,1)));
-                        var b = Number((draw.point.substr(1,1)));
-                        var c = Number((draw.point.substr(2,1)));
-                        var d = Number((draw.point.substr(3,2)));
-
-                        var obj = result.items[4].items[2];
-                        draw.skema = draw.encode(obj.query);
-
-                        editor.setValue(draw.skema);
-                        diagram = eval(draw.skema).format();
-                        diagram.addTo($('.diagram').get(0));
-
-                    });
+                    diagram = eval(draw.skema).format();
+                    diagram.addTo($('.diagram').get(0));
 
                 }
 
@@ -203,19 +167,21 @@ var draw = {
 
                 } else {
 
-                    var a = Number((this.id.substr(0,1)));
-                    var b = Number((this.id.substr(1,1)));
-                    var c = Number((this.id.substr(2,1)));
-                    var d = Number((this.id.substr(3,2)));
-                    var e = Number((this.id.substr(5,1)));
-                    var f = Number((this.id.substr(6,3)));
-
                     draw.type = 'sequence';
 
                 }
 
-                draw.point = this.id;
-                draw.diagram();
+                var jsonfile = '/' + this['jsonfeed'] + '?t=' + $.now();
+                jsonfile = jsonfile.replace('assets', this.id);
+                $("#json").attr("href", jsonfile);
+                
+                $.getJSON(jsonfile).done(function(result){
+
+                    var obj = result.items[4].items[2];
+                    draw.skema = draw.encode(obj.query);
+                    editor.setValue(draw.skema);
+
+                });
 
             });
 
