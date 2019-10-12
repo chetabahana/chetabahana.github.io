8119aa398e42e1b03a11d2d647854924c3495fe4
Chetabahana <chetabahana@gmail.com>
2019-10-12 20:28:48 +0700

    Update templatemo_script.js

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -1,34 +1,32 @@
 jQuery(function($) {
-	
+
     $(window).on('beforeunload', function(){
-	    
-	// to stick navbar on top and hash
-	$('.templatemo-top-menu').stickUp(); 
-	    
-    });
-	
+
+    // to stick navbar on top and hash
+    $('.templatemo-top-menu').stickUp();});
+
     $(window).on('load', function(){
-	    
-	// open links which point outside    
-	$(document.links).filter(function() {
+
+        // open links which point outside
+        $(document.links).filter(function() {
             return this.hostname != window.location.hostname;
-	}).attr('target', '_blank'); 
-	    
+        }).attr('target', '_blank'); 
+
         // do scroll and clear the hash anytime someone arrives with a hash tag    
         if( typeof(location.hash) !== 'undefined' && location.hash.length ) {
             scrollTo(location.hash);
-            history.replaceState(null, null, location.pathname); // https://stackoverflow.com/a/50688363/4058484				     
-	}
-	    
+            history.replaceState(null, null, location.pathname); // https://stackoverflow.com/a/50688363/4058484
+        }
+
     });
-		
+
     $(document).ready( function() {
 
-	// to stick navbar on top and hash
-	$('.templatemo-top-menu').stickUp(); 
-	    
-	// unbind external link   
-        $('.external-link').unbind('click');	    
+        // to stick navbar on top and hash
+        $('.templatemo-top-menu').stickUp(); 
+
+        // unbind external link
+        $('.external-link').unbind('click');
 
         // scroll spy to auto active the nav item
         top_menu_height = $('.templatemo-top-menu').height();
@@ -41,7 +39,7 @@ jQuery(function($) {
         });
 
         // scroll to specific id when click on link
-	$('.internal-link, .carousel-inner a').click(function(e){
+        $('.internal-link, .carousel-inner a').click(function(e){
             e.preventDefault(); 
             var linkId = $(this).attr('href');
             scrollTo(linkId);
@@ -59,6 +57,7 @@ jQuery(function($) {
             $(this).blur();
             return false;
         });
+
         //gallery light box setup
         $('a.colorbox').colorbox({
             rel: function(){
@@ -79,11 +78,12 @@ jQuery(function($) {
             });
             $(linkClass).fadeIn();  
         });
-	    
+
         // chetabahana-portfolio https://stackoverflow.com/a/50299022/4058484
         $(".templatemo-project-gallery").simplyScroll(); 
-	    
+
     });
+
 });
 
 // scrollTo 
@@ -92,7 +92,7 @@ function scrollTo(selectors)
 {
     if(!$(selectors).length) return;
     var selector_top = $(selectors).offset().top - top_menu_height;
-    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+    $('html,body').animate({scrollTop: selector_top }, 'slow');
 }
 
 /*juicerFeed
