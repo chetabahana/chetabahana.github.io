efcb8166b922289cbd32bbed5526c9febb28a413
Chetabahana <chetabahana@gmail.com>
2019-10-13 00:38:22 +0700

    Fix attribute on Workflow Diagrams  (#18)
    * loadingImg
    
    * Update Skema
    
    * Update Skema
    
    * Update Skema
    
    * Update Skema
    
    * Update Skema
    
    * Update Skema
    
    * Update Skema
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update Skema
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update Skema
    
    * Update Skema
    
    * Update styles.scss
    
    * status_left
    
    * Update Skema
    
    * Update styles.scss
    
    * display: table
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update Skema
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update Bottom
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update Bottom
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update Bottom
    
    * Update Skema
    
    * Update Skema
    
    * Update Head
    
    * Update Head
    
    *  <canvas></canvas>
    
    * Update Skema
    
    * prototype.js
    
    * Update templatemo_script.js
    
    * Update templatemo_script.js
    
    * Update Bottom
    
    * Update templatemo_script.js
    
    * skema_left skema_right
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update templatemo_script.js
    
    * Update templatemo_script.js
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update styles.scss

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -1,34 +1,34 @@
 jQuery(function($) {
-	
+
     $(window).on('beforeunload', function(){
-	    
-	// to stick navbar on top and hash
-	$('.templatemo-top-menu').stickUp(); 
-	    
+
+        // to stick navbar on top and hash
+        $('.templatemo-top-menu').stickUp();
+
     });
-	
+
     $(window).on('load', function(){
-	    
-	// open links which point outside    
-	$(document.links).filter(function() {
+
+        // open links which point outside
+        $(document.links).filter(function() {
             return this.hostname != window.location.hostname;
-	}).attr('target', '_blank'); 
-	    
-        // do scroll and clear the hash anytime someone arrives with a hash tag    
+        }).attr('target', '_blank'); 
+
+        // do scroll and clear the hash anytime someone arrives with a hash tag
         if( typeof(location.hash) !== 'undefined' && location.hash.length ) {
             scrollTo(location.hash);
-            history.replaceState(null, null, location.pathname); // https://stackoverflow.com/a/50688363/4058484				     
-	}
-	    
+            history.replaceState(null, null, location.pathname); // https://stackoverflow.com/a/50688363/4058484
+        }
+
     });
-		
+
     $(document).ready( function() {
 
-	// to stick navbar on top and hash
-	$('.templatemo-top-menu').stickUp(); 
-	    
-	// unbind external link   
-        $('.external-link').unbind('click');	    
+        // to stick navbar on top and hash
+        $('.templatemo-top-menu').stickUp(); 
+
+        // unbind external link
+        $('.external-link').unbind('click');
 
         // scroll spy to auto active the nav item
         top_menu_height = $('.templatemo-top-menu').height();
@@ -41,7 +41,7 @@ jQuery(function($) {
         });
 
         // scroll to specific id when click on link
-	$('.internal-link, .carousel-inner a').click(function(e){
+        $('.internal-link, .carousel-inner a').click(function(e){
             e.preventDefault(); 
             var linkId = $(this).attr('href');
             scrollTo(linkId);
@@ -59,6 +59,7 @@ jQuery(function($) {
             $(this).blur();
             return false;
         });
+
         //gallery light box setup
         $('a.colorbox').colorbox({
             rel: function(){
@@ -79,11 +80,12 @@ jQuery(function($) {
             });
             $(linkClass).fadeIn();  
         });
-	    
+
         // chetabahana-portfolio https://stackoverflow.com/a/50299022/4058484
         $(".templatemo-project-gallery").simplyScroll(); 
-	    
+
     });
+
 });
 
 // scrollTo 
@@ -92,10 +94,10 @@ function scrollTo(selectors)
 {
     if(!$(selectors).length) return;
     var selector_top = $(selectors).offset().top - top_menu_height;
-    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+    $('html,body').animate({scrollTop: selector_top }, 'slow');
 }
 
-//juicerFeed
+/*juicerFeed
 function juicerFeed() { 
     if (!$('#gfeeds').find('a')[0] || !$('#___community_0')) {
          window.requestAnimationFrame(juicerFeed);       
@@ -105,7 +107,7 @@ function juicerFeed() {
     }    
   } 
 
-//instafeed
+instafeed
 var feed = new Instafeed({
     get: 'user',
     limit: 100,
@@ -122,4 +124,5 @@ var feed = new Instafeed({
 
 });
 
-//feed.run();
+feed.run();
+*/
