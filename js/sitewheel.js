7bfbe92e7964e1f58df84e5267c71d1a1a5a1f33
Chetabahana <chetabahana@gmail.com>
2019-11-03 22:30:43 +0700

    Update sitewheel.js

--- js/sitewheel.js
+++ js/sitewheel.js
@@ -1,15 +1,15 @@
 function initialize (skema) {
-   
+
     var initPromise = $.Deferred();
-    var control = {};
-    control.divName = "#diagram";
+    var diagram = {};
+    diagram.divName = "#diagram";
     
     //some basic options
     var newoptions = {  nodeLabel:"label", 
                     nodeResize:"count", height:700,
                     nodeFocus:true, radius:3, charge:-500};
     // defaults
-    control.options = $.extend({
+    diagram.options = $.extend({
             stackHeight : 12,
             radius : 5,
             fontSize : 14,
@@ -18,7 +18,7 @@ function initialize (skema) {
             nodeLabel : null,
             markerWidth : 0,
             markerHeight : 0,
-            width : $(control.divName).outerWidth(),
+            width : $(diagram.divName).outerWidth(),
             gap : 1.5,
             nodeResize : "",
             linkDistance : 80,
@@ -36,58 +36,58 @@ function initialize (skema) {
             circleFill: "Black",
             routeStroke: "Black",
             routeStrokeWidth: 1,
-            height : $(control.divName).outerHeight()
+            height : $(diagram.divName).outerHeight()
            
         }, newoptions);
         
-    var options = control.options;
+    var options = diagram.options;
     options.gap = options.gap * options.radius;
-    control.width = options.width;
-    control.height = options.height;
+    diagram.width = options.width;
+    diagram.height = options.height;
     // this is an element that can be used to determine the width of a text label
     
-    control.scratch = $(document.createElement('span'))
+    diagram.scratch = $(document.createElement('span'))
         .addClass('shadow')
         .css('display','none')
-        .css("font-size",control.options.labelFontSize + "px");   
-    $('body').append(control.scratch);
+        .css("font-size",diagram.options.labelFontSize + "px");   
+    $('body').append(diagram.scratch);
     
-    getPromise(control,skema).then( function (data) {  
+    getPromise(diagram,skema).then( function (data) {  
         
-        control.data = data;
-        control.nodes = data.nodes;
-        control.links = data.links;
-        control.color = d3.scale.category20();
-        control.clickHack = 200;
+        diagram.data = data;
+        diagram.nodes = data.nodes;
+        diagram.links = data.links;
+        diagram.color = d3.scale.category20();
+        diagram.clickHack = 200;
     
-        control.svg = d3.select(control.divName)
+        diagram.svg = d3.select(diagram.divName)
             .append("svg:svg")
-            .attr("width", control.width)
-            .attr("height", control.height);
+            .attr("width", diagram.width)
+            .attr("height", diagram.height);
         
-        control.force = d3.layout.force().
-            size([control.width, control.height])
-            .linkDistance(control.options.linkDistance)
-            .charge(control.options.charge)
-            .gravity(control.options.gravity);
+        diagram.force = d3.layout.force().
+            size([diagram.width, diagram.height])
+            .linkDistance(diagram.options.linkDistance)
+            .charge(diagram.options.charge)
+            .gravity(diagram.options.gravity);
     
-       initPromise.resolve(control);
+       initPromise.resolve(diagram);
     });
     return initPromise.promise();
 }
 
-  function doTheTreeViz(control) {
+  function doTheTreeViz(diagram) {
 
-    var svg = control.svg;
+    var svg = diagram.svg;
 
-    var force = control.force;
-    force.nodes(control.nodes)
-        .links(control.links)
+    var force = diagram.force;
+    force.nodes(diagram.nodes)
+        .links(diagram.links)
         .start();
 
     // Update the links
     var link = svg.selectAll("line.link")
-        .data(control.links, function(d) { 
+        .data(diagram.links, function(d) { 
             return d.key; 
         } );
  
@@ -107,7 +107,7 @@ function initialize (skema) {
 
   // Update the nodes
     var node = svg.selectAll("g.node")
-        .data(control.nodes, function(d) { return d.key; });
+        .data(diagram.nodes, function(d) { return d.key; });
 
     node.select("circle")
         .style("fill", function(d) {
@@ -123,22 +123,22 @@ function initialize (skema) {
         .attr("class", "node")
         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
         .on("dblclick", function(d){
-            control.nodeClickInProgress=false;
+            diagram.nodeClickInProgress=false;
             if (d.url) draw.click(d);
         })
         .on("click", function(d){
             // this is a hack so that click doesnt fire on the1st click of a dblclick
-            if (!control.nodeClickInProgress ) {
-                control.nodeClickInProgress = true;
+            if (!diagram.nodeClickInProgress ) {
+                diagram.nodeClickInProgress = true;
                 setTimeout(function(){
-                    if (control.nodeClickInProgress) { 
-                        control.nodeClickInProgress = false;
-                        if (control.options.nodeFocus) {
+                    if (diagram.nodeClickInProgress) { 
+                        diagram.nodeClickInProgress = false;
+                        if (diagram.options.nodeFocus) {
                             d.isCurrentlyFocused = !d.isCurrentlyFocused;
-                            doTheTreeViz(makeFilteredData(control));
+                            doTheTreeViz(makeFilteredData(diagram));
                         }
                     }
-                },control.clickHack); 
+                },diagram.clickHack); 
             }
         })
         .call(force.drag);
@@ -161,46 +161,46 @@ function initialize (skema) {
             
         })
       .append("svg:title")
-        .text(function(d) { return d[control.options.nodeLabel]; })
+        .text(function(d) { return d[diagram.options.nodeLabel]; })
         
     function enhanceNode(selectedNode) {
         link.filter ( function (d) {
             return d.source.key == selectedNode.key || d.target.key == selectedNode.key;
         } )
-        .style("stroke", control.options.routeFocusStroke)
-        .style("stroke-width", control.options.routeFocusStrokeWidth);
+        .style("stroke", diagram.options.routeFocusStroke)
+        .style("stroke-width", diagram.options.routeFocusStrokeWidth);
         
         if (text) {
             text.filter ( function (d) {
                 return areWeConnected (selectedNode,d);
             } )
-            .style("fill", control.options.routeFocusStroke);
+            .style("fill", diagram.options.routeFocusStroke);
         }
     }
     
     function areWeConnected (node1,node2) {
-        for (var i=0; i < control.data.links.length ; i++) {
-            var lnk = control.data.links[i];
+        for (var i=0; i < diagram.data.links.length ; i++) {
+            var lnk = diagram.data.links[i];
             if ( (lnk.source.key === node1.key && lnk.target.key === node2.key) ||
                  (lnk.source.key === node2.key && lnk.target.key === node1.key) ) return lnk;
         }
         return null;
     }
     function resetNode(selectedNode) {
-        link.style("stroke", control.options.routeStroke)
-            .style("stroke-width", control.options.routeStrokeWidth);
+        link.style("stroke", diagram.options.routeStroke)
+            .style("stroke-width", diagram.options.routeStrokeWidth);
         if (text) { 
-            text.style("fill", control.options.routeStroke);
+            text.style("fill", diagram.options.routeStroke);
         }
     }
 
-   if (control.options.nodeLabel) {       
+   if (diagram.options.nodeLabel) {       
        // text is done once for shadow as well as for text
         var textShadow = nodeEnter.append("svg:text")
             .attr("x", function(d) {
                 var x = (d.right || !d.fixed) ? 
-                    control.options.labelOffset : 
-                    (-d.dim.width - control.options.labelOffset)  ;
+                    diagram.options.labelOffset : 
+                    (-d.dim.width - diagram.options.labelOffset)  ;
                 return x;
             })
             .attr("dy", ".31em")
@@ -208,7 +208,7 @@ function initialize (skema) {
             .attr("text-anchor", function(d) { 
                 return !d.right ? 'start' : 'start' ;
             })
-            .style("font-size",control.options.labelFontSize + "px")
+            .style("font-size",diagram.options.labelFontSize + "px")
             .text(function(d) {
                 return d.shortName ? d.shortName : d.name;
             });
@@ -216,15 +216,15 @@ function initialize (skema) {
         var text = nodeEnter.append("svg:text")
             .attr("x", function(d) {
                 var x = (d.right || !d.fixed) ? 
-                    control.options.labelOffset : 
-                    (-d.dim.width - control.options.labelOffset)  ;
+                    diagram.options.labelOffset : 
+                    (-d.dim.width - diagram.options.labelOffset)  ;
                 return x;
             })
             .attr("dy", ".35em")
             .attr("class", "text")
             .attr("text-anchor", function(d) { 
                 return !d.right ? 'start' : 'start' ;})
-            .style("font-size",control.options.labelFontSize + "px")
+            .style("font-size",diagram.options.labelFontSize + "px")
             .text(function(d) {
                 return d.shortName ? d.shortName : d.name;
             })
@@ -233,7 +233,7 @@ function initialize (skema) {
             // enhance all the links that end here
                 enhanceNode (d);
                 d3.select(this)
-                    .style('fill',control.options.routeFocusStroke);
+                    .style('fill',diagram.options.routeFocusStroke);
             })
         
             .on("mouseout", function(d){
@@ -243,16 +243,16 @@ function initialize (skema) {
 
     // Exit any old nodes.
     node.exit().remove();
-    control.link = svg.selectAll("line.link");
-    control.node = svg.selectAll("g.node");
+    diagram.link = svg.selectAll("line.link");
+    diagram.node = svg.selectAll("g.node");
     force.on("tick", tick);
 
 
 
-    if (control.options.linkName) {
+    if (diagram.options.linkName) {
         link.append("title")
             .text(function(d) {
-                return d[control.options.linkName];
+                return d[diagram.options.linkName];
         });
     }
 
@@ -269,26 +269,26 @@ function initialize (skema) {
     }
  
     function getRadius(d) {
-        return makeRadius(control,d);
+        return makeRadius(diagram,d);
     }
     function getColor(d) {
-        return control.options.nodeFocus && d.isCurrentlyFocused ? control.options.nodeFocusColor  : control.color(d.group) ;
+        return diagram.options.nodeFocus && d.isCurrentlyFocused ? diagram.options.nodeFocusColor  : diagram.color(d.group) ;
     }
 
    }
    
-function makeRadius(control,d) {
-     var r = control.options.radius * (control.options.nodeResize ? Math.sqrt(d[control.options.nodeResize]) / Math.PI : 1);
-     return control.options.nodeFocus && d.isCurrentlyFocused ? control.options.nodeFocusRadius  : r;
+function makeRadius(diagram,d) {
+     var r = diagram.options.radius * (diagram.options.nodeResize ? Math.sqrt(d[diagram.options.nodeResize]) / Math.PI : 1);
+     return diagram.options.nodeFocus && d.isCurrentlyFocused ? diagram.options.nodeFocusRadius  : r;
 }
 
-function makeFilteredData(control,selectedNode){
+function makeFilteredData(diagram,selectedNode){
     // we'll keep only the data where filterned nodes are the source or target
     var newNodes = [];
     var newLinks = [];
 
-    for (var i = 0; i < control.data.links.length ; i++) {
-        var link = control.data.links[i];
+    for (var i = 0; i < diagram.data.links.length ; i++) {
+        var link = diagram.data.links[i];
         if (link.target.isCurrentlyFocused || link.source.isCurrentlyFocused) {
             newLinks.push(link);
             addNodeIfNotThere(link.source,newNodes);
@@ -297,14 +297,14 @@ function makeFilteredData(control,selectedNode){
     }
     // if none are selected reinstate the whole dataset
     if (newNodes.length > 0) {
-        control.links = newLinks;
-        control.nodes = newNodes;
+        diagram.links = newLinks;
+        diagram.nodes = newNodes;
     }
     else {
-        control.nodes = control.data.nodes;
-        control.links = control.data.links;
+        diagram.nodes = diagram.data.nodes;
+        diagram.links = diagram.data.links;
     }
-    return control;
+    return diagram;
     
     function addNodeIfNotThere( node, nodes) {
         for ( var i=0; i < nodes.length; i++) {
@@ -320,12 +320,12 @@ function getPixelDims(scratch,t) {
     scratch.append(document.createTextNode(t));
     return { width: scratch.outerWidth(), height: scratch.outerHeight() } ;
 }
-function getPromise(control,data) {
+function getPromise(diagram,data) {
     //var dataPromise = getTheRawData();
     var massage = $.Deferred();
     //dataPromise.done ( function (data) {
         // need to massage it
-        massage.resolve ( dataMassage (control,data));    
+        massage.resolve ( dataMassage (diagram,data));    
     //})
     //.fail (function (error) {
         //console.log (error);
@@ -334,7 +334,7 @@ function getPromise(control,data) {
     return massage.promise();
 }
 
-function dataMassage(control,data) {
+function dataMassage(diagram,data) {
 
 var ind = data, nodes = [],links =[];
    // the tags are to be circles
@@ -344,7 +344,7 @@ var ind = data, nodes = [],links =[];
        // add links to pages
        for ( var j=0; j < ind[i].pages.length; j++) {
            //push this page as a node
-           var node = findOrAddPage(control,ind[i].pages[j],nodes);
+           var node = findOrAddPage(diagram,ind[i].pages[j],nodes);
            node.isCurrentlyFocused = false;
            // create a link
            var link = { source:node , target:ind[i], key : node.key + "_" + ind[i].key };
@@ -353,35 +353,35 @@ var ind = data, nodes = [],links =[];
    }
    // sort nodes alpha
    nodes.sort ( function (a,b) { return a.name < b.name  ? -1 : (a.name == b.name ? 0 : 1 ) ; });
-   control.pageCount = 0;
-   control.pageRectSize = {width:0,height:0,radius:0};   
+   diagram.pageCount = 0;
+   diagram.pageRectSize = {width:0,height:0,radius:0};   
    for ( var i = 0; i < nodes.length ; i++) {
        page= nodes[i];
        page.group =0;
-       page.dim = getPixelDims(control.scratch, page.name);
+       page.dim = getPixelDims(diagram.scratch, page.name);
        if (page.fixed) { 
-           control.pageCount++;
+           diagram.pageCount++;
           // this will calculate the width/height in pixels of the largest label
-           control.pageRectSize.width = Math.max(control.pageRectSize.width,page.dim.width);
-           control.pageRectSize.height = Math.max(control.pageRectSize.height,page.dim.height);
-           control.pageRectSize.radius = Math.max(control.pageRectSize.radius,makeRadius(control,page));
+           diagram.pageRectSize.width = Math.max(diagram.pageRectSize.width,page.dim.width);
+           diagram.pageRectSize.height = Math.max(diagram.pageRectSize.height,page.dim.height);
+           diagram.pageRectSize.radius = Math.max(diagram.pageRectSize.radius,makeRadius(diagram,page));
            page.group =1;    
        }
        
    }
-   var options= control.options;
+   var options= diagram.options;
 
    // we're going to fix the nodes that are pages into two columns
     for ( var i = 0, c=0; i < nodes.length ; i++) {
         var page = nodes[i];
         if (page.fixed) {
-            page.right= (c > control.pageCount/2);
+            page.right= (c > diagram.pageCount/2);
             // y dimension calc same for each column
-            page.y = ((c % (control.pageCount/2)) + .5) * (control.pageRectSize.height)  ;
+            page.y = ((c % (diagram.pageCount/2)) + .5) * (diagram.pageRectSize.height)  ;
             
             // x based on right or left column
             page.x = page.right ? 
-                        control.width - control.pageRectSize.width - options.labelOffset  :
+                        diagram.width - diagram.pageRectSize.width - options.labelOffset  :
                         page.dim.width + options.labelOffset ;
             c++;
         }
@@ -392,7 +392,7 @@ var ind = data, nodes = [],links =[];
 
 }
 
-function findOrAddPage(control,page,nodes) {
+function findOrAddPage(diagram,page,nodes) {
     for ( var i=0;i<nodes.length;i++) {
         if ( nodes[i].key === page.key ) { 
             nodes[i].count++;
