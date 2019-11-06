3fed785dbdcbf566b62e85b3e5868fcaa7b0ab3e
Chetabahana <chetabahana@gmail.com>
2019-11-06 12:44:42 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -35,8 +35,8 @@ jQuery(function($) {
         // to stick navbar on top and hash
         // scroll spy to auto active the nav item
         $('.templatemo-top-menu').stickUp(); 
-        top_menu_height = $('.templatemo-top-menu').height();
-        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
+        top_menu_height = $('.templatemo-top-menu').height() + 10;
+        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height});
 
         // do scroll and clear the hash anytime someone arrives with a hash tag
         // https://stackoverflow.com/a/50688363/4058484
