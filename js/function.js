a7a8c8cdcaecd4b971edbe13d0faa8049a5bb7e5
Chetabahana <chetabahana@gmail.com>
2019-11-22 13:38:25 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -20,7 +20,7 @@ jQuery(function($)
         // do scroll and clear the hash anytime someone arrives with a hash tag
         // https://stackoverflow.com/a/50688363/4058484
         if( typeof(location.hash) !== 'undefined' && location.hash.length ) {
-            var location_hash = location.hash;
+            var location_hash = location.hash;console.log(location_hash);
             history.replaceState(null, null, location.pathname);
             scrollTo(location_hash);
         }
