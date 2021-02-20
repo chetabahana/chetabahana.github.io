68a3dbc9a340129adfd563187acfbe22c3d0f952
Chetabahana <chetabahana@gmail.com>
2021-02-20 08:28:53 +0700

    relocate

---
+++ _layouts/redirect.js
@@ -0,0 +1,13 @@
+---
+layout: data
+---
+{% comment %}
+*
+*  This redirects are performed by serving an HTML file with an HTTP-REFRESH
+*  meta tag which points to the destination via '_layout/data'
+*  which configured via variable {{ page.redirect.from }}
+*
+*  You may see the repository code here:
+*  https://github.com/jekyll/jekyll-redirect-from
+*
+{%- endcomment -%}
