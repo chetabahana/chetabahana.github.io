38d6540f62cc4ebe8cff8810089bf2ad4aa2efec
Chetabahana <chetabahana@gmail.com>
2021-02-22 03:02:19 +0700

    include data.rb

--- _layouts/feeds/section/skema.rb
+++ _layouts/feeds/section/skema.rb
@@ -1,5 +1,4 @@
 ---
 layout: default
 ---
-{%- assign feed = site.feeds | where_exp:'item', 'item.redirect_from contains page.redirect.from' -%}
-{%- assign my_feed = feed[0] %}{%- include feeds/section.rb -%}
+{%- include feeds/section.rb -%}
