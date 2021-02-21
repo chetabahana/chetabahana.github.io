ba9629fa42b0ab026bb5c9aeab83b33c5a51c5fc
Chetabahana <chetabahana@gmail.com>
2021-02-22 06:25:01 +0700

    data.items[0]

--- _layouts/default.rb
+++ _layouts/default.rb
@@ -10,7 +10,7 @@
 {% endcomment %}{% include data.rb %}{% if page.path != 'index.html' and page.ext != '.md' %}{
     "id": {% if data.id %}{{ data.id }}{% else %}{{ page.id }}{% endif %},
     "name": {% if data.name %}{{ data.name }}{% else %}"{{ site.name | xml_escape }}"{% endif %},
-    "version": "https://jsonfeed.org/version/1",
+    "version": "https://jsonfeed.org/version/1.1",
     "home_page_url": "{{ "/" | absolute_url }}",
     "feed_url": {% if data.feed_url %}"{{ data.feed_url }}"{% else %}"{{ page.url | absolute_url }}"{% endif %},
     "title": {% if data.title %}"{{ data.title }}"{% else %}"{{ site.title | xml_escape }}"{% endif %},
