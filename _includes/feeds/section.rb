977939c301b810bc8e2001c6558595dab24c6fb5
Chetabahana <chetabahana@gmail.com>
2021-02-21 07:18:29 +0700

    Update section.rb

--- _includes/feeds/section.rb
+++ _includes/feeds/section.rb
@@ -6,8 +6,8 @@
 *
 *  collect matched data
 *
-{% endcomment %}{% assign data = null %}{% assign my_data = "" | split: "," %}{% for item in site.data %}{% if item[1].title != "primes" %}{% assign my_data = my_data | push:item %}{% endif %}{% endfor %}{% assign my_hash = my_feed.path | split: '/' | pop %}{% for hash in my_hash %}{% if hash contains '_' %}{% assign my_data = my_data[hash] %}{% else %}{% assign _hash = hash | prepend: '_' %}{% assign my_data = my_data[_hash] %}{% endif %}{% endfor %}{% for hash in my_data %}{% if hash[1].title == my_feed.slug %}{% assign data = hash[1] %}{% endif %}{% endfor %}{% assign my_tab = "    " %}{% assign my_data = null %}{% comment %}
+{% endcomment %}{% assign data = null %}{% assign my_data = site.data %}{% assign my_hash = my_feed.path | split: '/' | pop %}{% for hash in my_hash %}{% if hash contains '_' %}{% assign my_data = my_data[hash] %}{% else %}{% assign _hash = hash | prepend: '_' %}{% assign my_data = my_data[_hash] %}{% endif %}{% endfor %}{% for hash in my_data %}{% if hash[1].title == my_feed.slug %}{% assign data = hash[1] %}{% endif %}{% endfor %}{%- assign my_tab = "    " -%}{% comment %}
 *
 *  get page for inclusion
 *
-{% endcomment %}{% if my_feed.category %}{% include {{ my_feed.path | remove_first: "_" | replace: ".md", ".html" }} %}{%- endif -%}
+{% endcomment %}{% if my_feed.category %}{% include {{ my_feed.path | remove_first: "_" | replace: ".md", ".html" }} %}{% endif %}
