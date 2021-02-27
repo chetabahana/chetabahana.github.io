214d02fc50a4bbc58550328ebd6f1a160bf90d42
Chetabahana <chetabahana@gmail.com>
2021-02-27 13:01:32 +0700

    Update data.rb

--- _includes/data.rb
+++ _includes/data.rb
@@ -2,7 +2,7 @@
 *
 *  collect feed
 *
-{% endcomment %}{% if page.redirect.from %}{% assign feed = site.leads | where_exp:'item', 'item.redirect_from contains page.redirect.from' %}{% assign my_feed = feed[0] %}{% else %}{% assign my_feed = page %}{% endif %}{% comment %}
+{% endcomment %}{% if page.redirect.from %}{% assign feed = site.leads | where_exp:'item', 'item.redirect_from contains page.redirect.from' %}{% assign my_feed = feed[0] %}{"assets": {{ my_feed | jsonify  }}}{% else %}{% assign my_feed = page %}{% endif %}{% comment %}
 *
 *  collect property
 *
