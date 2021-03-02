addb636f03a564b1bccbec56accced4d3ee6237a
Chetabahana <chetabahana@gmail.com>
2021-03-03 02:50:31 +0700

    import 'coffeescript'

--- _layouts/coffee.rb
+++ _layouts/coffee.rb
@@ -5,9 +5,32 @@
 *
 *  https://coffeescript.org/
 *  https://jekyllrb.com/docs/assets/
+*  https://coffeescript.org/v1/annotated-source/browser.html
+*  https://github.com/chetabahana/grammar/blob/master/examples/implementation_languages/coffeescript/coffeescript_json.coffee
 *
 *  You may see the running code here:
 *  https://chetabahana.github.io/try.js
 *
 {%- endcomment =%}
+import './local-file.coffee'
+import 'coffeescript'
 
+import _ from 'underscore'
+import * as underscore from 'underscore'
+
+import { now } from 'underscore'
+import { now as currentTimestamp } from 'underscore'
+import { first, last } from 'underscore'
+import utilityBelt, { each } from 'underscore'
+
+export default Math
+export square = (x) -> x * x
+export class Mathematics
+  least: (x, y) -> if x < y then x else y
+
+export { sqrt }
+export { sqrt as squareRoot }
+export { Mathematics as default, sqrt as squareRoot }
+
+export * from 'underscore'
+export { max, min } from 'underscore'
