9fd742cd27d88c2a08361b8929e9c08e110e2781
Chetabahana <chetabahana@gmail.com>
2021-03-04 15:40:50 +0700

    Update coffee.rb

--- _layouts/coffee.rb
+++ _layouts/coffee.rb
@@ -3,7 +3,7 @@
 *  Jekyll provides built-in support for Sass and can work with CoffeeScript via a Ruby gem.
 *  https://jekyllrb.com/docs/assets/
 *  https://coffeescript.org/#modules
-*  https://coffeescript.org/announcing-coffeescript-2/
+*  https://coffeescript.org/v1/annotated-source/browser.html
 *
 *  You may see the running code here:
 *  https://chetabahana.github.io/try.js
@@ -12,7 +12,7 @@
 *
 *  ES2015 modules are supported in CoffeeScript, with very similar import and export syntax:
 *  https://github.com/jashkenas/coffeescript/issues/3162
-*  https://coffeescript.org/v1/annotated-source/browser.html
+*  https://github.com/chetabahana/chetabahana.github.io/blob/Chetabahana/package.json#L2
 *  https://github.com/chetabahana/grammar/blob/master/examples/implementation_languages/coffeescript/coffeescript_json.coffee
 *
 {%- endcomment =%}
