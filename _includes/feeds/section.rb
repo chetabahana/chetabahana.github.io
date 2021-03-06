795fd105a5855a0850ee8bc98075724224a4ad44
Chetabahana <chetabahana@gmail.com>
2021-03-06 13:09:26 +0700

    Update section.rb

--- _includes/feeds/section.rb
+++ _includes/feeds/section.rb
@@ -4,5 +4,5 @@
 *
 {%- endcomment -%}
 {%- if data == null -%}{%- include data.rb -%}{%- endif -%}
-{%- if my_feed.category == null or my_feed.redirect.from == null %}{{ my_feed.content }}{%- else -%}
+{%- if my_feed.category == null and my_feed.redirect.from == null %}{{ my_feed.content }}{%- else -%}
 {%- include {{ my_feed.path | remove_first: "_" | replace: ".md", ".html" }} -%}{%- endif -%}
