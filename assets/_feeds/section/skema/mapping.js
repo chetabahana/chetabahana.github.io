9ca42fa1033f33a71dba2fd6f544d33ba24f4d73
Chetabahana <chetabahana@gmail.com>
2021-03-07 14:13:36 +0700

    mapping.js

--- assets/_feeds/section/skema/mapping.js
+++ assets/_feeds/section/skema/mapping.js
@@ -10,7 +10,4 @@ frame: [50,60,400,70,10,50]
 object: [1,9,26,40,56,74,82,96]
 permalink: /mapping.js
 ---
-mass = 72
-height = 1.78
-BMI = mass / height**2
-alert 'You are healthy!' if 18.5 < BMI < 25
+{{ data.items[0] | jsonify }}
