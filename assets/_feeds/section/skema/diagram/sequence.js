a7a632607d477322e5136dd47cc816b6fd00443a
Chetabahana <chetabahana@gmail.com>
2021-03-07 20:54:01 +0700

    Update sequence.js

--- assets/_feeds/section/skema/diagram/sequence.js
+++ assets/_feeds/section/skema/diagram/sequence.js
@@ -11,6 +11,15 @@ frame: [1,30,90,200,9]
 object: [1,4,13,22]
 permalink: /diagram/sequence.js
 ---
+{% comment %}
+*
+*  Here we are defining The Document Object Model (DOM) with JavaScript
+*  as the relations between their nodes with our feeds object.
+*
+*  You may see the reference of the code here:
+*  https://javascript.plainenglish.io/how-to-traverse-the-dom-in-javascript-d6555c335b4e
+*
+{%- endcomment -%}
 var Sequence = {
 
     feed : function(id, size) {
