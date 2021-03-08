d504e1f0ce09d9731798f3013c180dfaabae4350
Chetabahana <chetabahana@gmail.com>
2021-03-08 07:01:03 +0700

    Update mapping.js

--- assets/_feeds/section/skema/mapping.js
+++ assets/_feeds/section/skema/mapping.js
@@ -10,5 +10,14 @@ frame: [50,60,400,70,10,50]
 object: [1,9,26,40,56,74,82,96]
 permalink: /mapping.js
 ---
+{% comment %}
+*
+*  Here we are defining The Document Object Model (DOM) with JavaScript
+*  as the relations between their nodes with our feeds object.
+*
+*  You may see the reference of the code here:
+*  https://chetabahana.github.io/mapping.js?t=1234
+*
+{%- endcomment -%}
 {"data": {{ data[0] | jsonify }}}
 {"roots": {{ my_roots | jsonify }}}
