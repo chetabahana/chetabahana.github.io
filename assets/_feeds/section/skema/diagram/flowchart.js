71db76e7fbdb8ae625d1d4f4aba8fa5dba730640
Chetabahana <chetabahana@gmail.com>
2021-03-09 23:08:59 +0700

    permalink: /diagram/flowchart.js

--- assets/_feeds/section/skema/diagram/flowchart.js
+++ assets/_feeds/section/skema/diagram/flowchart.js
@@ -10,6 +10,7 @@ value: [919,3,-1,-2]
 frame: [1,5,4,50,1]
 object: [1,15,17,20,24,29]
 redirect_from: /flowchart.json
+permalink: /diagram/flowchart.js
 ---
 {% comment %}
 *
@@ -17,5 +18,16 @@ redirect_from: /flowchart.json
 *
 *  You may see the running code here:
 *  https://chetabahana.github.io/flowchart.json
+*  https://chetabahana.github.io/diagram/flowchart.js
 *
 {% endcomment %}
+var Flowchart = {
+
+    feed : function(id, size) {
+
+        if (!data) feed = '/flowchart.json?t=' + $.now();
+        draw.getJSON();
+
+    }
+
+}
