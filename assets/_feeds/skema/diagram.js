1f2e923241f0c0691f15de175221bd7ae1001fe8
Chetabahana <chetabahana@gmail.com>
2021-03-10 20:20:55 +0700

    category: skema

--- assets/_feeds/skema/diagram.js
+++ assets/_feeds/skema/diagram.js
@@ -5,7 +5,6 @@ key: 49
 pos: 67
 weight: 3
 primes: 4
-category: skema
 value: [719,0,-1,1]
 object: [1,6,13,21,28,42,60,75,82,88]
 permalink: /diagram.js
--- assets/_feeds/skema/mapping.js
+++ assets/_feeds/skema/mapping.js
@@ -4,7 +4,6 @@ key: 46
 pos: 156
 weight: 2
 primes: 640
-category: skema
 value: [911,4,-1,-2]
 frame: [50,60,400,70,10,50]
 object: [1,9,26,40,56,74,82,96]
