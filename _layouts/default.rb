be94745b0e363ff3b233023d74fac01b92afc992
Chetabahana <chetabahana@gmail.com>
2021-03-10 13:09:46 +0700

    skema

--- _layouts/default.rb
+++ _layouts/default.rb
@@ -33,7 +33,7 @@
 *
 *  capture each outputs
 *
-{% endcomment %}{% assign my_captures = "" | split: "," %}{% for feed in my_paths %}{% assign my_split = feed.path | split: "/" %}{% assign my_path_size = my_split | size %}{% assign my_part = my_split | size | minus:2 %}{% assign my_size = my_split | size | times:2 | minus:4 %}{% assign my_dir = my_split[my_part] | prepend: "/" | append: "/" %}{% assign my_title = feed.title | downcase | prepend: my_dir | append: "/" %}{% assign my_index = "" | split: "," %}{% for my_push in my_paths %}{% assign my_num = my_push.path | split: "/" | size | minus:my_path_size %}{% if my_num == 1 and my_push.path contains my_title %}{% assign my_index = my_index | push:forloop.index0 %}{% endif %}{% endfor %}{% comment %}
+{% endcomment %}{% assign my_captures = "" | split: "," %}{% for feed in my_paths %}{% assign my_split = feed.path | split: "/" %}{% assign my_path_size = my_split | size %}{% assign my_part = my_split | size | minus: 2 %}{% assign my_size = my_split | size | times: 2 | minus: 4 %}{% assign my_dir = my_split[my_part] | prepend: "/" | append: "/" %}{% assign my_title = feed.title | downcase | prepend: my_dir | append: "/" %}{% assign my_index = "" | split: "," %}{% for my_push in my_paths %}{% assign my_num = my_push.path | split: "/" | size | minus:my_path_size %}{% if my_num == 1 and my_push.path contains my_title %}{% assign my_index = my_index | push:forloop.index0 %}{% endif %}{% endfor %}{% comment %}
 *
 *  index the feeds output
 *
