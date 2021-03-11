86e45d22e3e9c492f76289536ca346160dd99552
Chetabahana <chetabahana@gmail.com>
2021-03-11 10:11:58 +0700

    Update diagram.js

--- assets/_feeds/skema/diagram.js
+++ assets/_feeds/skema/diagram.js
@@ -271,8 +271,8 @@ var id, js, ids, pad, back, data, feed, json, link, init, size, test, type, quer
             // Underscore.js : findWhere with nested property value
             // ref https://stackoverflow.com/a/21600748/4058484
             if (!json) var filtered = _.filter(result.items, function(obj) {
-                return _.where(obj.items, {id: 157}).length > 0;
-            });console.log(filtered);
+                return _.some(obj.items, {id: 128});
+            });console.log(_.where(filtered, { items:[{id:128}] }));
 
             if (!json) json = result.items[4].items[2].items;
 
