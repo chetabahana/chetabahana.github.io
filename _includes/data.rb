763286cd547d106ef70531e182e50505d252c2ba
Chetabahana <chetabahana@gmail.com>
2021-03-13 03:33:48 +0700

    Update data.rb

--- _includes/data.rb
+++ _includes/data.rb
@@ -6,4 +6,4 @@
 *
 *  collect property
 *
-{% endcomment %}{% assign data = null %}{% assign my_data = site.data %}{% assign my_hash = my_feed.path | remove_first: '_' | replace: page.ext | '' | split: '/' %}{% for hash in my_hash %}{% assign my_data = my_data[hash] %}{% endfor %}{% for hash in my_data %}{% if hash[1].id == my_feed.slug %}{% assign data = hash[1] %}{% endif %}{% endfor %}
+{% endcomment %}{% assign data = null %}{% assign my_data = site.data %}{% assign my_hash = my_feed.path | remove_first: '_' | split: '/' | pop %}{% for hash in my_hash %}{% assign my_data = my_data[hash] %}{% endfor %}{% for hash in my_data %}{% if hash[1].id == my_feed.slug %}{% assign data = hash[1] %}{% endif %}{% endfor %}
