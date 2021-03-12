93c3974ed592dce89d924104db86f5506a238095
Chetabahana <chetabahana@gmail.com>
2021-03-12 23:21:52 +0700

    Update data.rb

--- _includes/data.rb
+++ _includes/data.rb
@@ -6,7 +6,7 @@
 *
 *  collect property
 *
-{% endcomment %}{% assign my_size = my_feed.path | split: '/' | size | plus: 1 %}{% assign my_path = my_feed.path | split: '/' | pop | join: '/' | append: '/' | append: my_feed.slug | append: '/' %}{% assign feeds = site.feeds | sort: 'weight' | where_exp:'item', 'item.path contains my_path' %}{% for feed in feeds %}{% assign feed_size = feed.path | split: '/' | size %}{% endfor %}{% comment %}
+{% endcomment %}{% assign my_size = my_feed.path | split: '/' | size | plus: 1 %}{% assign my_path = my_feed.path | split: '/' | pop | join: '/' | append: '/' | append: my_feed.slug | append: '/' %}{% assign feeds = site.feeds | sort: 'weight' | where_exp:'item', 'item.path contains my_path' %}{% comment %}
 *
 *  collect mathed data
 *
