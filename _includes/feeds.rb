0e43f839505939f030478d9b83135c3653593649
Chetabahana <chetabahana@gmail.com>
2021-03-13 07:19:46 +0700

    Update feeds.rb

--- _includes/feeds.rb
+++ _includes/feeds.rb
@@ -3,7 +3,7 @@
 *  get page for inclusion
 *
 {%- endcomment -%}
-{%- include data.rb -%}{%- endif -%}
+{%- include data.rb -%}
 {%- if my_feed.category == null and my_feed.redirect.from == null %}{{ my_feed.content }}
 {%- elsif my_slug -%}{%- include {{ my_feed.path | remove_first: "_" | replace: my_feed.ext, ".html" }} -%}
 {%- elsif my_feed.category == 'section' %}{%- include {{ my_feed.path | remove_first: "_" | replace: my_feed.ext, ".html" }} -%}
