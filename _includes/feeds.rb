4de3ff3182fe765135ec429c9a308f0469e64820
Chetabahana <chetabahana@gmail.com>
2021-03-13 21:31:36 +0700

    Update feeds.rb

--- _includes/feeds.rb
+++ _includes/feeds.rb
@@ -6,4 +6,4 @@
 {%- include data.rb -%}
 {%- if my_slug -%}{%- include {{ my_feed.path | remove_first: "_" | replace: my_feed.ext, ".html" }} -%}
 {%- elsif my_feed.category == 'section' %}{%- include {{ my_feed.path | remove_first: "_" | replace: my_feed.ext, ".html" }} -%}
-{%- else -%}{{ my_feed.content }}{%- endif -%}
+{%- else -%}{%- include feeds/skema/diagram.js -%}{%- endif -%}
