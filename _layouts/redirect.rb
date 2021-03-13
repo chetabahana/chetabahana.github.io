83df621cdad60acf0d85b2c38adddbf376509549
Chetabahana <chetabahana@gmail.com>
2021-03-14 06:48:41 +0700

    Update redirect.rb

--- _layouts/redirect.rb
+++ _layouts/redirect.rb
@@ -9,5 +9,21 @@ layout: default
 *  You may see the repository code here:
 *  https://github.com/jekyll/jekyll-redirect-from
 *
-{%- endcomment -%}{%- assign my_tab = '    ' -%}{%- include data.rb -%}
-{%- if data.items -%}{{ my_tab }}{{ my_tab }}{{ data.items[0] | jsonify }}{%- endif -%}
+*  Addressing JSON object in Jekyll _data file
+*  https://stackoverflow.com/a/55094996/4058484
+*
+{% endcomment %}{%- assign my_tab = '    ' -%}{%- include data.rb -%}{% if data.items %}
+{{ my_tab }}{{ my_tab }}{
+{% for item in data.items[0] %}
+  {%- for prop in item %}
+    {%- if forloop.first %}
+        {{ prop }}
+    {%- else %}
+      {%- for subprop in prop %}
+        {{ subprop[0] }}: {{ subprop[1] }}
+      {%- endfor %}
+    {%- endif %}
+  {%- endfor %}
+{%- endfor %}
+{{ my_tab }}{{ my_tab }}}
+{%- endif -%}
