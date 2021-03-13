1712288cf0f68352db624a174f3020cd0c692dc5
Chetabahana <chetabahana@gmail.com>
2021-03-13 16:48:50 +0700

    Update feeds.rb

--- _includes/feeds.rb
+++ _includes/feeds.rb
@@ -4,7 +4,6 @@
 *
 {%- endcomment -%}
 {%- include data.rb -%}
-{%- if my_feed.category == null and my_feed.redirect.from == null %}{{ my_feed.content }}
-{%- elsif my_slug -%}{%- include {{ my_feed.path | remove_first: "_" | replace: my_feed.ext, ".html" }} -%}
+{%- if my_slug -%}{%- include {{ my_feed.path | remove_first: "_" | replace: my_feed.ext, ".html" }} -%}
 {%- elsif my_feed.category == 'section' %}{%- include {{ my_feed.path | remove_first: "_" | replace: my_feed.ext, ".html" }} -%}
 {%- else -%}{{ my_feed.content }}{%- endif -%}
