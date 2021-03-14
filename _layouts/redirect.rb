9d992871d202bf5562e7c02fd8f384782ad47140
Chetabahana <chetabahana@gmail.com>
2021-03-14 11:22:00 +0700

    Update redirect.rb

--- _layouts/redirect.rb
+++ _layouts/redirect.rb
@@ -3,11 +3,12 @@ layout: default
 ---
 {% comment %}
 *
-*  This redirects are performed by serving an HTML file with an HTTP-REFRESH
+*  This redirects are performed by serving a data file with an HTTP-REFRESH
 *  meta tag which configured via variable {{ page.redirect.from }}
+*  Ref: https://github.com/jekyll/jekyll-redirect-from
 *
-*  You may see the repository code here:
-*  https://github.com/jekyll/jekyll-redirect-from
+*  You may see the running code here:
+*  https://chetabahana.github.io/sequence.json
 *
 *  Addressing JSON object in Jekyll _data file
 *  https://stackoverflow.com/a/55094996/4058484
@@ -15,7 +16,9 @@ layout: default
 {% endcomment %}{% include data.rb %}{%- if data.items -%}
 {% assign my_tab = '    ' %}{{ my_tab }}{{ my_tab }}{
 {%- for item in data.items[0] -%}
-            {{ item | jsonify }}
+    {%- for prop in item %}
+            {{ prop | jsonify }}
+    {%- endfor %}
 {%- endfor %}
         }
 {%- endif -%}
