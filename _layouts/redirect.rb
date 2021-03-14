f40c4c44415b45d5d38405fa23bac12529c02458
Chetabahana <chetabahana@gmail.com>
2021-03-14 10:14:04 +0700

    Update redirect.rb

--- _layouts/redirect.rb
+++ _layouts/redirect.rb
@@ -18,7 +18,7 @@ layout: default
     {%- for prop in item %}
         {%- if forloop.first %}
             {{ prop | jsonify }}: {
-        {%- else %}
+        {%- else %}{{ prop.size | times: 1 }}{{ prop | size | times: 2 }}{{ prop[0].size | times: 3}}{{ prop[0] | size | times: 4}}{{ prop | jsonify | size | times: 5}}{{ prop[0] | jsonify | size | times: 5}}
             {%- if prop | size == 1 %}
                 {{ prop | jsonify }}
             {%- else %}
