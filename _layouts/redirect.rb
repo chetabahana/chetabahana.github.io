15980e23e96c2eef90b9c7e5223fca97a41499f3
Chetabahana <chetabahana@gmail.com>
2021-03-15 01:33:23 +0700

    Update redirect.rb

--- _layouts/redirect.rb
+++ _layouts/redirect.rb
@@ -14,18 +14,19 @@ layout: default
 *  https://stackoverflow.com/q/34048313/4058484
 *
 {% endcomment %}{% include data.rb %}{% if data.items -%}
-    {%- assign variable = data.items[0] %}{% assign my_tabs = 2 -%}
-    {%- assign my_tab = '    ' %}{% capture my_tabs %}
-    {% for i in (1..my_tabs) %}{{ my_tab }}{% endfor %}{% endcapture -%}
+    {%- assign variable = data.items[0] %}{% assign my_tabs = 2 -%}{%- assign my_tab = '    ' -%}
+    {%- capture my_tabs %}{% for i in (1..my_tabs) %}{{ my_tab }}{% endfor %}{% endcapture -%}
+    {%- capture my_tabn %}
+{{ my_tabs }}{% endcapture -%}
     {{- my_tabs }}{
     {%- for items in variable %}
         {%- if items[1].first %}
             {%- for item in items %}
                 {%- if forloop.first %}
-                    {{- my_tabs }}{{ item | jsonify }}:
+                    {{- my_tabn }}{{ item | jsonify }}:
                 {%- else %}
                     {%- for subitem in item %}
-                        {{- my_tabs }}{{ subitem[0] | jsonify}}: {{ subitem[1] | jsonify}}
+                        {{- my_tabn }}{{ my_tab }}{{ subitem[0] | jsonify}}: {{ subitem[1] | jsonify}}
                     {%- endfor %}
                 {%- endif %}
             {%- endfor %}
@@ -36,5 +37,5 @@ layout: default
             {{- my_tabs }}{{ items[0] | jsonify }}: {{ items[1] | jsonify }}
         {%- endif %}{% unless forloop.last %},{% endunless %}
     {%- endfor -%}
-    {{- my_tabs }}}
+    {{- my_tabn }}}
 {%- endif -%}
