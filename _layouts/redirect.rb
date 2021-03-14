0f0f537dfbbc2c3d97e4101d87747fb721734ede
Chetabahana <chetabahana@gmail.com>
2021-03-14 11:48:12 +0700

    Update redirect.rb

--- _layouts/redirect.rb
+++ _layouts/redirect.rb
@@ -17,8 +17,8 @@ layout: default
 {% assign my_tab = '    ' %}{{ my_tab }}{{ my_tab }}{
 {%- for items in data.items[0] -%}
     {%- for item in items %}
-        {% assign my_size = item[1] | jsonify | split: ',' | size | times: 1 -%}
-        {%- if my_size == 1 %}
+        {% assign my_item_size = item[1] | jsonify | split: ',' | size | times: 1 -%}
+        {%- if my_item_size == 1 %}
             {{ item[0] | jsonify }}: {{ item[1] | jsonify }}{% unless forloop.last %},{% endunless %}
         {%- else %}
         {%- for prop in item %}
