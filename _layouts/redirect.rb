85a77b75efb1dfca3422c265f24434396e67c8f0
Chetabahana <chetabahana@gmail.com>
2021-03-14 22:19:15 +0700

    jsonify | escape

--- _layouts/redirect.rb
+++ _layouts/redirect.rb
@@ -10,18 +10,18 @@ layout: default
 *  You may see the running code here:
 *  https://chetabahana.github.io/sequence.json
 *
-*  Addressing JSON object in Jekyll _data file
-*  https://stackoverflow.com/a/55094996/4058484
+*  jekyll debug or print all variables
+*  
 *
 {% endcomment %}{% include data.rb %}{%- if data.items -%}
 {% assign my_tab = '    ' %}{{ my_tab }}{{ my_tab }}{
 {%- for items in data.items[0] -%}
         {%- if items[1].first %}
-            {{ items[0] | jsonify }}: {
+            {{ items[0] | jsonify | escape }}: {
 
             }
         {%- else %}
-            {{ items[0] | jsonify }}: {{ items[1] | jsonify -}}
+            {{ items[0] | jsonify  | escape }}: {{ items[1] | jsonify | escape -}}
         {%- endif -%}{% unless forloop.last %},{% endunless %}
 {%- endfor %}
         }
