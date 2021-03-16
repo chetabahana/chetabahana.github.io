6bc6ac55a9c127fff8e03a7fdf5527feca5dded1
Chetabahana <chetabahana@gmail.com>
2021-03-16 12:02:36 +0700

    Update data.rb

--- _includes/data.rb
+++ _includes/data.rb
@@ -4,6 +4,10 @@
 *
 {% endcomment %}{% if page.redirect.from %}{% assign my_slug = page.redirect.from | split: '/' | last | split: '.' | first | prepend: '/' %}{% assign feed = site.feeds | where_exp:'item', 'item.path contains my_slug' %}{% assign my_feed = feed[0] %}{% else %}{% assign my_feed = page %}{% endif %}{% comment %}
 *
-*  collect property
+*  collect hash
 *
-{% endcomment %}{% assign my_data = site.data %}{% assign my_datas = '' | split: ',' %}{% assign my_hash = my_feed.path | remove_first: '_' | split: '/' | pop %}{% for hash in my_hash %}{% assign _hash = hash | prepend: '_' %}{% assign my_data = my_data[_hash] %}{% assign my_datas = my_datas | push: my_data %}{% endfor %}{% for hash in my_datas %}{% if hash[1].id == my_feed.myId %}{% assign data = hash[1] %}{% endif %}{%- endfor -%}
+{% endcomment %}{% assign my_data = site.data %}{% assign my_datas = '' | split: ',' %}{% assign my_hash = my_feed.path | remove_first: '_' | split: '/' | pop %}{{my_hash|jsonify}}{% for hash in my_hash %}{% assign _hash = hash | prepend: '_' %}{% assign my_data = my_data[_hash] %}{% assign my_datas = my_datas | push: my_data %}{% endfor %}{% comment %}
+*
+*  collect data
+*
+{% endcomment %}{% for hash in my_datas %}{% if hash[1].id == my_feed.myId %}{% assign data = hash[1] %}{% endif %}{%- endfor -%}
