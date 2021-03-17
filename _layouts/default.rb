034fd2739a0a6e875307dbd9195265cc2339171f
Chetabahana <chetabahana@gmail.com>
2021-03-18 04:09:43 +0700

    Update default.rb

--- _layouts/default.rb
+++ _layouts/default.rb
@@ -24,12 +24,12 @@
 *  index the feeds output
 *
 {% endcomment %}{% capture my_capture %}
-{% if page.path != 'index.html' %}{% include items.rb %}{% endif %}{% for i in (1..my_size) %}{{ my_tab }}{% endfor %}}{% endcapture %}{% assign my_captures = my_captures | push: my_capture %}{% endfor %}{% assign my_feeds = '' | split: ',' %}{% for feed in feeds %}{% assign my_dir = feed.path | split: '/' | pop | join: '/' %}{% if my_dir == my_base %}{% assign my_feeds = my_feeds | push: feed %}{% endif %}{% endfor %}{% for feed in my_feeds %}{% assign my_title = feed.title | downcase | prepend: '/' | prepend: my_base | append: '/' %}{% assign my_index = '' | split: ',' %}{% for my_path in my_paths %}{% assign my_split = my_path.path | split: '/' | size %}{% if my_split == 3 and my_path.path contains my_title %}{% assign my_index = my_index | push: forloop.index0 %}{% endif %}{% endfor %}{% assign my_size = 2 %}{% comment %}
+{% if page.path != 'index.html' %}{% include items.rb %}{% endif %}{% for i in (1..my_size) %}{{ my_tab }}{% endfor %}}{% endcapture %}{% assign my_captures = my_captures | push: my_capture %}{% endfor %}{% assign my_feeds = '' | split: ',' %}{% for feed in feeds %}{% assign my_dir = feed.path | split: '/' | pop | join: '/' %}{% if my_dir == my_base %}{% assign my_feeds = my_feeds | push: feed %}{% endif %}{% endfor %}{% for feed in my_feeds %}{% assign my_title = feed.title | downcase | prepend: '/' | prepend: my_base | append: '/' %}{% assign my_index = '' | split: ',' %}{% for my_path in my_paths %}{% assign my_split = my_path.path | split: '/' | size %}{% if my_split == 3 and my_path.path contains my_title %}{% assign my_index = my_index | push: forloop.index0 %}{% endif %}{% endfor %}{% comment %}
 *
 *  render the feeds output
-*  <pre>JSON.pretty_generate(@data)</pre>
+*  JSON.pretty_generate(@data)
 *
-{% endcomment %}{% if page.path == 'feed.json' %}
+{% endcomment %}{% assign my_size = 2 %}{% if page.path == 'feed.json' %}
 {% include items.rb %}{{ my_tab }}{{ my_tab }}}{% unless forloop.last %},{% endunless %}{% endif %}{% endfor %}{% if page.path != 'index.html' and page.ext != '.md' %}
 {{ my_tab }}]
 }
