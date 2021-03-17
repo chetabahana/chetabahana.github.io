d3196ab14624181fd84be9fee1b915600105c5f2
Chetabahana <chetabahana@gmail.com>
2021-03-17 22:09:29 +0700

    Update feeds.rb

--- _includes/feeds.rb
+++ _includes/feeds.rb
@@ -16,5 +16,17 @@
 {%- endcomment -%}
 {%- if data.css and my_feed.ext == '.css' %}{%- include {{ data.css }} -%}
 {%- elsif data.js and my_feed.ext == '.js' %}{%- include {{ data.js }} -%}
-{%- elsif data.id and my_feed.ext == '.json' %}{{ data | jsonify }}
+{%- elsif data.id and my_feed.ext == '.json' %}{
+    {{ 'id' | jsonify }}: {% if data.id %}{{ data.id }}{% else %}{{ page.id }}{% endif %},
+    {{ 'title' | jsonify }}: "{% if data.title %}{{ data.title | capitalize }} | {% endif %}Project Maps",
+    {{ 'version' | jsonify }}: {{ 'https://jsonfeed.org/version/1.1' | jsonify }},
+    {{ 'home_page_url' | jsonify }}: {{ '/' | absolute_url | jsonify }},
+    {{ 'feed_url' | jsonify }}: {{ page.url | absolute_url | jsonify }},
+    {{ 'description' | jsonify }}: {{ site.slogan | jsonify }},
+    {{ 'user_comment' | jsonify }}: {{ 'This feed allows you to read the project mapping' | jsonify }},
+    {{ 'items' | jsonify }}: [
+        {{{ data.items[0] | jsonify }}},
+        {"test": {{ site.slogan | jsonify}}}
+    ]
+}
 {%- else -%}{{ my_feed.content }}{% endif %}
