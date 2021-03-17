ca5806480ab622808ba3dda1426e8f785e317f47
Chetabahana <chetabahana@gmail.com>
2021-03-17 23:48:47 +0700

    Update default.rb

--- _layouts/default.rb
+++ _layouts/default.rb
@@ -7,20 +7,6 @@
 *  You may see the running code here:
 *  https://chetabahana.github.io/feed.json
 *
-{% endcomment %}{% if data == null %}{% include data.rb %}{% endif -%}
-{%- if page.path != 'index.html' and page.ext != '.md' %}{
-    {{ 'id' | jsonify }}: {% if data.id %}{{ data.id }}{% else %}{{ page.id }}{% endif %},
-    {{ 'title' | jsonify }}: "{% if data.title %}{{ data.title | capitalize }} | {% endif %}Project Maps",
-    {{ 'version' | jsonify }}: {{ 'https://jsonfeed.org/version/1.1' | jsonify }},
-    {{ 'home_page_url' | jsonify }}: {{ '/' | absolute_url | jsonify }},
-    {{ 'feed_url' | jsonify }}: {{ page.url | absolute_url | jsonify }},
-    {{ 'description' | jsonify }}: {{ site.slogan | jsonify }},
-    {{ 'user_comment' | jsonify }}: {{ 'This feed allows you to read the project mapping' | jsonify }},
-    {{ 'items' | jsonify }}: [
-{% endif %}{% comment %}
-*
-*  assign the required variables
-*
 {% endcomment %}{% assign my_base = site.basedir %}{% assign my_atributs = 'id, key, link, weight, title, next, content, output, previous, redirect_from, relative_path, collection, excerpt, category, categories, ext, layout, tags, path, url, draft, slug, pos, myId, type, primes, value, object, frame, ratio, pairs, zones' | split:', ' %}{% assign my_tab = '    ' %}{% assign my_path = '' | split: ',' %}{% comment %}
 *
 *  get the maximum size of feeds
