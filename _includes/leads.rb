9631cb6f0792ebd82ff30a154da3c513cbad77d3
Chetabahana <chetabahana@gmail.com>
2021-03-19 00:01:48 +0700

    include leads.rb

---
+++ _includes/leads.rb
@@ -0,0 +1,10 @@
+{%- comment -%}
+*
+*  get lead for feeds object inclusion
+*
+*  ref: 
+*  https://docs.treepl.co/liquid
+*  https://stackoverflow.com/a/53693728/4058484
+*
+{%- endcomment -%}
+test: 1
