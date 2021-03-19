63e4cc314c0faddb7a53c04313be7fdd26766a31
Chetabahana <chetabahana@gmail.com>
2021-03-19 21:59:57 +0700

    Update roots.rb

--- _includes/data/roots.rb
+++ _includes/data/roots.rb
@@ -228,30 +228,24 @@
 {%- assign n = 0 %}
 {%- assign my_keys = '' | split: ',' %}
 {%- assign my_rows = '' | split: ',' %}
-{%- assign my_node = '' | split: ',' %}
 {%- assign my_nodes = '' | split: ',' %}
 {%- for item in my_roots limit: my_pos %}
     {%- assign size = item.pos | size | minus: 2 %}
     {%- assign my_nodes_pos = item.pos | slice: 0, size %}
     {%- if n < 139 -%}
         {%- unless my_keys contains my_nodes_pos %}
-            {%- assign my_rows = my_rows | push: forloop.index0 %}
             {%- assign my_keys = my_keys | push: my_nodes_pos %}
-            {%- assign my_nodes = my_nodes | push: my_node %}
-            {%- assign my_node = '' | split: ',' %}
+            {%- assign my_rows = my_rows | push: forloop.index0 %}
         {%- endunless %}
     {%- endif %}
     {%- assign nodes = item.node | split: ';' %}
     {%- for node in nodes %}
         {%- assign n = n | plus: 1 %}
-        {%- assign node = node | times: 1 %}
-        {%- assign my_node = my_node | push: node %}
         {%- if n != my_pos -%}
             {%- continue -%}
         {%- else -%}
-            {%- assign my_debug = my_nodes | size %}
-            {%- assign my_node = node %}
-            {% break -%}
+            {%- assign my_debug = my_rows | size %}
+            {%- assign my_node = node | times: 1 %}{% break -%}
         {%- endif %}
     {%- endfor -%}
 {%- endfor -%}
