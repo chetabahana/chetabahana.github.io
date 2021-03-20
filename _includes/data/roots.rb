0a019a4751adaa4005a5c5734f22a7a306db5d6f
Chetabahana <chetabahana@gmail.com>
2021-03-21 05:23:44 +0700

    Update roots.rb

--- _includes/data/roots.rb
+++ _includes/data/roots.rb
@@ -8,32 +8,32 @@
     {%- assign my_frame = nil %}
     {%- assign size = item.pos | size | minus: 2 %}
     {%- assign my_nodes_pos = item.pos | slice: 0, size %}
-    {%- if n <= 139 %}
-        {%- unless my_keys contains my_nodes_pos %}
-            {%- assign my_rows = my_rows | push: forloop.index0 %}
-            {%- assign my_keys = my_keys | push: my_nodes_pos %}
-            {%- assign my_nodes = my_nodes | push: my_node %}
-            {%- assign my_node = '' | split: ',' %}
-        {%- endunless %}
-    {%- elsif my_pos > 139 %}
+    {%- unless my_keys contains my_nodes_pos %}
+        {%- assign my_rows = my_rows | push: forloop.index0 %}
+        {%- assign my_keys = my_keys | push: my_nodes_pos %}
+        {%- assign my_nodes = my_nodes | push: my_node %}
+        {%- assign my_node = '' | split: ',' %}
+    {%- endunless %}
+    {%- if my_pos > 139 %}
         {%- comment %}{% endcomment %}{% assign my_debug = my_nodes %}
         {%- assign my_frame_pos = my_pos | minus: 139 %}
         {%- assign my_frame = my_nodes[my_frame_pos] %}
         {%- assign my_root = nil %}
         {%- break %}
+    {%- else %}
+        {%- assign nodes = item.node | split: ';' %}
+        {%- for node in nodes %}
+            {%- assign n = n | plus: 1 %}
+            {%- assign my_obj = node | times: 1 %}
+            {%- assign my_node = my_node | push: my_obj %}
+            {%- if n == my_pos %}
+                {%- assign my_root = node | times: 1 %}
+                {%- assign my_frame = nil %}
+                {%- break %}
+            {%- else -%}
+                {%- continue %}
+            {%- endif %}
+        {%- endfor %}
+        {%- if my_root %}{% break %}{% endif %}
     {%- endif %}
-    {%- assign nodes = item.node | split: ';' %}
-    {%- for node in nodes %}
-        {%- assign n = n | plus: 1 %}
-        {%- assign my_obj = node | times: 1 %}
-        {%- assign my_node = my_node | push: my_obj %}
-        {%- if n == my_pos %}
-            {%- assign my_root = node | times: 1 %}
-            {%- assign my_frame = nil %}
-            {%- break %}
-        {%- else -%}
-            {%- continue %}
-        {%- endif %}
-    {%- endfor %}
-    {%- if my_root %}{% break %}{% endif %}
 {%- endfor -%}
