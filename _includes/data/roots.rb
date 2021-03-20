18c52321da9ab9be8519bdbc2d86cb271ebbf683
Chetabahana <chetabahana@gmail.com>
2021-03-21 03:54:11 +0700

    Update roots.rb

--- _includes/data/roots.rb
+++ _includes/data/roots.rb
@@ -8,7 +8,12 @@
     {%- assign my_frame = nil %}
     {%- assign size = item.pos | size | minus: 2 %}
     {%- assign my_nodes_pos = item.pos | slice: 0, size %}
-    {%- if n < 139 %}
+    {%- if n > 139 %}
+        {%- assign my_frame_pos = my_pos | minus: 139 %}
+        {%- assign my_frame = my_nodes[my_frame_pos] %}
+        {%- assign my_root = nil %}
+        {%- break %}
+    {%- else %}
         {%- unless my_keys contains my_nodes_pos %}
             {%- comment %}{% assign my_debug = my_nodes %}{% endcomment %}
             {%- assign my_rows = my_rows | push: forloop.index0 %}
@@ -16,11 +21,6 @@
             {%- assign my_nodes = my_nodes | push: my_node %}
             {%- assign my_node = '' | split: ',' %}
         {%- endunless %}
-    {%- else %}
-        {%- assign my_frame_pos = my_pos | minus: 139 %}
-        {%- assign my_frame = my_nodes[my_frame_pos] %}
-        {%- assign my_root = nil %}
-        {%- break %}
     {%- endif %}
     {%- assign nodes = item.node | split: ';' %}
     {%- for node in nodes %}
