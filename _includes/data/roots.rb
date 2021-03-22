16478dc0655483566c3629a5245886e237410ae8
Chetabahana <chetabahana@gmail.com>
2021-03-23 02:20:32 +0700

    myId >= 68 and myId <= 67

--- _includes/data/roots.rb
+++ _includes/data/roots.rb
@@ -25,8 +25,7 @@
     {%- elsif myId >= 29 and myId <= 40 %}{%- assign my_debug = myId | plus: 8 -%}
     {%- elsif myId >= 41 and myId <= 45 %}{%- assign my_debug = myId | plus: 27 -%}
     {%- elsif myId >= 46 and myId <= 50 %}{%- assign my_debug = myId | minus: 35 -%}
-    {%- elsif myId >= 51 and myId <= 67 %}{%- assign my_debug = myId | minus: 72 -%}
-    {%- elsif myId >= 68 and myId <= 67 %}{%- assign my_debug = myId | minus: 72 -%}
+    {%- elsif myId >= 51 and myId <= 75 %}{%- assign my_debug = myId | minus: 72 -%}
     {%- elsif myId >= 76 and myId <= 77 %}{%- assign my_debug = 105 | minus: myId -%}
     {%- elsif myId >= 78 and myId <= 80 %}{%- assign my_debug = myId | minus: 53 -%}
     {%- elsif myId >= 81 and myId <= 85 %}{%- assign my_debug = myId | minus: 61 -%}
