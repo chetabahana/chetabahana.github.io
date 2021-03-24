8f50e6a8e4deafc4e8f39b05809dc7430c2658fd
Chetabahana <chetabahana@gmail.com>
2021-03-25 06:15:55 +0700

    update _includes/data/roots.rb
    files related to this commit are:
    _config.yml
    _includes/items.rb
    _layouts/feed.rb
    .github/hook-scripts/StartCommit.js
    _includes/data/roots.rb
    
    documentation are available in bahasa:
    https://github.com/chetabahana/chetabahana.github.io/wiki
    https://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid

--- .github/hook-scripts/StartCommit.js
+++ .github/hook-scripts/StartCommit.js
@@ -1,23 +1,47 @@
-/* this script is a local start-commit hook script.
-ref: https://stackoverflow.com/a/64723713/4058484 */
+/* 
+this script is a local start-commit hook script.
 
-var fs = new ActiveXObject("Scripting.FileSystemObject");
+ref: 
+https://stackoverflow.com/a/64723713/4058484
+https://www.robvanderwoude.com/vbstech_data_environment.php
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+https://docs.microsoft.com/en-us/previous-versions//4yyeyb0a(v=vs.85)
+*/
+
+var line;
 
 var ForReading = 1;
 var ForWriting = 2;
-var objArgs, num;
 
-objArgs = WScript.Arguments;
-num = objArgs.length;
-if (num !== 3)
+var objArgs = WScript.Arguments;
+var ws = new ActiveXObject("WScript.Shell");
+var fs = new ActiveXObject("Scripting.FileSystemObject");
+
+if (objArgs.length !== 3)
 {
     WScript.Echo("Usage: [CScript | WScript] StartCommit.js path/to/pathsfile path/to/messagefile path/to/CWD ");
     WScript.Quit(1);
 }
 
+function readPaths(path)
+{
+    var retPaths = [];
+    if (fs.FileExists(path))
+    {
+        var a = fs.OpenTextFile(path, ForReading);
+        while (!a.AtEndOfStream)
+        {
+            line = a.ReadLine();
+            retPaths.push(line);
+        }
+        a.Close();
+    }
+    return retPaths;
+}
+
 var paths = readPaths(objArgs(0));
-var message = "update " + WScript.ScriptName;
-message = message + "\n\nfiles related to this commit are:\n";
+var subject = "update " + line;
+var message = "files related to this commit are:\n";
 
 for (var i = 0; i < paths.length; i++)
 {
@@ -29,23 +53,6 @@ message = message + "\nhttps://github.com/chetabahana/chetabahana.github.io/wiki
 message = message + "\nhttps://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid";
 
 var w = fs.OpenTextFile(".github/hook-scripts/git_message.txt", ForWriting);
-/* WScript.Echo(message); */
-w.WriteLine(message);
+w.WriteLine(subject); w.WriteBlankLines(1); w.WriteLine(message);
 
 WScript.Quit(0);
-
-function readPaths(path)
-{
-    var retPaths = [];
-    if (fs.FileExists(path))
-    {
-        var a = fs.OpenTextFile(path, ForReading);
-        while (!a.AtEndOfStream)
-        {
-            var line = a.ReadLine();
-            retPaths.push(line);
-        }
-        a.Close();
-    }
-    return retPaths;
-}
