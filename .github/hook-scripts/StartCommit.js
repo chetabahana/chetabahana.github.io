5d32b873a242c6219e449f7fb9efa02a3daa8cca
Chetabahana <chetabahana@gmail.com>
2021-03-25 02:11:34 +0700

    list of paths
    selected files for commit:
    _config.yml
    _includes/items.rb
    _layouts/feed.rb
    _layouts/default.rb
    _includes/data/roots.rb
    .github/hook-scripts/StartCommit.js
    .gitignore
    .github/hook-scripts/git_message.txt
    path of message file is: C:\Users\CHETAB~1\AppData\Local\Temp\TortoiseGit\git369E.tmp
    CWD is: C:\Users\Chetabahana\Documents\GitHub\Concepts\FrontEnd\github.io

---
+++ .github/hook-scripts/StartCommit.js
@@ -0,0 +1,49 @@
+/* this script is a local start-commit hook script.
+ref: https://stackoverflow.com/a/64723713/4058484 */
+
+var fs = new ActiveXObject("Scripting.FileSystemObject");
+
+var ForReading = 1;
+var ForWriting = 2;
+var objArgs, num;
+
+objArgs = WScript.Arguments;
+num = objArgs.length;
+if (num !== 3)
+{
+    WScript.Echo("Usage: [CScript | WScript] StartCommit.js path/to/pathsfile path/to/messagefile path/to/CWD ");
+    WScript.Quit(1);
+}
+
+var paths = readPaths(objArgs(0));
+var message = "list of paths\n\nselected files for commit:\n";
+
+for (var i = 0; i < paths.length; i++)
+{
+    message = message + paths[i] + "\n";
+}
+
+message = message + "path of message file is: " + objArgs(1) + "\n";
+message = message + "CWD is: " + objArgs(2) + "\n";
+
+var w = fs.OpenTextFile(".github/hook-scripts/git_message.txt", ForWriting);
+/* WScript.Echo(message); */
+w.WriteLine(message);
+
+WScript.Quit(0);
+
+function readPaths(path)
+{
+    var retPaths = [];
+    if (fs.FileExists(path))
+    {
+        var a = fs.OpenTextFile(path, ForReading);
+        while (!a.AtEndOfStream)
+        {
+            var line = a.ReadLine();
+            retPaths.push(line);
+        }
+        a.Close();
+    }
+    return retPaths;
+}
