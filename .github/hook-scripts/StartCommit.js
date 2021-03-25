ba780e402d2fbf4b62dae3a971d31d8cfe498154
Chetabahana <chetabahana@gmail.com>
2021-03-25 11:08:13 +0700

    Update .github/hook-scripts/StartCommit.js
    Files related to this commit are:
    _config.yml
    _includes/items.rb
    _layouts/feed.rb
    _includes/data/roots.rb
    _layouts/default.rb
    .github/hook-scripts/StartCommit.js
    
    Documentation are available in bahasa:
    https://github.com/chetabahana/chetabahana.github.io/wiki
    https://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid

--- .github/hook-scripts/StartCommit.js
+++ .github/hook-scripts/StartCommit.js
@@ -60,6 +60,7 @@ message = message + "\nhttps://github.com/chetabahana/chetabahana.github.io/wiki
 
 /* WScript.Echo(sh.ExpandEnvironmentStrings("%path%"));*/
 /* sh.Run("git config commit.template .github/hook-scripts/git_message.txt", 0);;*/
+
 var w = fs.OpenTextFile(".github/hook-scripts/git_message.txt", ForWriting);
 w.WriteLine(subject); w.WriteBlankLines(1); w.WriteLine(message); w.Close();
 
