7f8ff66968a98eb6827b3ff3fff84271520c6cd8
Chetabahana <chetabahana@gmail.com>
2021-03-29 04:48:20 +0700

    Update roots.rb

--- _includes/data/roots.rb
+++ _includes/data/roots.rb
@@ -12,7 +12,7 @@
 {%- endcomment -%}
 {%- assign my_debug = nil %}
 {%- assign my_value = '' | split: ',' %}
-{%- assign my_count = my_roots | where_exp:"item", "item.key < 2014" | last %}
+{%- assign my_count = my_roots.key | where_exp:"item", "item < 2014" | last %}
 {%- case myId %}
     {%- when 9 %}{%- assign my_debug = 100 -%}
     {%- when 13 %}{%- assign my_debug = 110 -%}
