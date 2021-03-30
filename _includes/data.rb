f0b6a32ebdb7b69b787b62043dbacedee98a799c
Chetabahana <chetabahana@gmail.com>
2021-03-31 03:57:17 +0700

    Update data.rb

--- _includes/data.rb
+++ _includes/data.rb
@@ -12,11 +12,14 @@
 *
 {% endcomment %}{% if my_feed.myId > 114 %}{% assign myId = my_feed.myId | modulo: 100 %}{% else %}{% assign myId = my_feed.myId %}{% endif %}{% for hashs in my_datas %}{% for hash in hashs %}{% if hash[1].id == myId %}{% assign data = hash[1] %}{% endif %}{% endfor %}{%- endfor -%}{% comment %}
 *
-*  collect json
+*  collect item
 *
 {% endcomment %}{% if page.redirect.from or my_feed.path == 'feed.json' or my_feed.ext == '.json' %}{
-    {{ 'id' | jsonify }}: {% if data.id %}{{ data.id }}{% else %}{{ page.id }{% assign my_content = content | remove: '[' | remove: ']'  | split: ',' %}{% assign my_count = '' | split: ',' %}{% for item in my_content %}{% assign citem = item | times: 1 %}{% assign my_count = my_count | push: citem %}{% endfor %}{% endif %},
-    {{ 'title' | jsonify }}: "{% if data.title %}{{ data.title | capitalize }} | {% endif %}Project Maps",
+    {{ 'id' | jsonify }}: {% if data.id %}{{ data.id }}{% else %}{{ page.id }}{% assign my_content = content | remove: '[' | remove: ']'  | split: ',' %}{% assign my_count = '' | split: ',' %}{% for item in my_content %}{% assign citem = item | times: 1 %}{% assign my_count = my_count | push: citem %}{% endfor %}{% endif %},{% comment %}
+*
+*  collect json
+*
+{% endcomment %}{{ 'title' | jsonify }}: "{% if data.title %}{{ data.title | capitalize }} | {% endif %}Project Maps",
     {{ 'version' | jsonify }}: {{ 'https://jsonfeed.org/version/1.1' | jsonify }},
     {{ 'home_page_url' | jsonify }}: {{ '/' | absolute_url | jsonify }},
     {{ 'feed_url' | jsonify }}: {{ page.url | absolute_url | jsonify }},
