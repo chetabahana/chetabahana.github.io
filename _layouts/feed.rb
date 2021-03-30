343d8a1876b5d15be1dc3e52c5890ad1adf689be
Chetabahana <chetabahana@gmail.com>
2021-03-31 06:25:39 +0700

    Update _layouts/feed.rb
    Files related to this commit are:
    _layouts/feed.rb
    
    Documentation are available in bahasa:
    https://github.com/chetabahana/chetabahana.github.io/wiki
    https://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid

--- _layouts/feed.rb
+++ _layouts/feed.rb
@@ -176,7 +176,7 @@ assets: [29,6116,3481,3747,2804,3050,3320,1233,2498,1833,1917,1777,853,830,655,1
     {%- for j in my_primes %}
         {%- if i < j %}
             {%- continue %}
-        {%- endif %}
+        {%- else %}
             {%- assign k = i | minus: forloop.index %}
             {%- break %}
         {%- endif %}
