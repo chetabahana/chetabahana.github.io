d42176acebc03733aab027d1a72312c3367cf910
Chetabahana <chetabahana@gmail.com>
2021-03-31 01:30:30 +0700

    my_primes | jsonify
    Files related to this commit are:
    _layouts/feed.rb
    
    Documentation are available in bahasa:
    https://github.com/chetabahana/chetabahana.github.io/wiki
    https://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid

--- _layouts/feed.rb
+++ _layouts/feed.rb
@@ -619,4 +619,4 @@ assets: [29,6116,3481,3747,2804,3050,3320,1233,2498,1833,1917,1777,853,830,655,1
 *  https://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid
 *
 {%- endcomment -%}
-{{- my_loops | jsonify -}}
+{{- my_primes | jsonify -}}
