af5bedeb5b4612bfc9a1fbe7974c06b775bb854f
Chetabahana <chetabahana@gmail.com>
2021-03-30 12:47:46 +0700

    Update _layouts/feed.rb
    Files related to this commit are:
    _layouts/feed.rb
    
    Documentation are available in bahasa:
    https://github.com/chetabahana/chetabahana.github.io/wiki
    https://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid

--- _layouts/feed.rb
+++ _layouts/feed.rb
@@ -148,6 +148,14 @@ assets: [29,6116,3481,3747,2804,3050,3320,1233,2498,1833,1917,1777,853,830,655,1
 *  ------|------|-----+-----+------
 *
 {%- endcomment -%}
+{%- assign n = 0 %}
+{%- for item in my_roots %}
+    {%- assign n = n | plus: 1 %}
+    {%- assign vkey = item.key | times: 1 %}
+    {%- if vkey < 10 %}
+        {%- assign my_loops = n %}
+    {%- endif -%}
+{%- endfor %}
 {%- assign my_loop = π10 -%}
 {%- assign my_loop = my_loop | plus: π100 %}
 {%- comment -%}
@@ -225,14 +233,6 @@ assets: [29,6116,3481,3747,2804,3050,3320,1233,2498,1833,1917,1777,853,830,655,1
 *   3:4:9 | 30 |3000 | 71 = 68 + log 10³ ---------¤         Δ900   
 *
 {%- endcomment -%}
-{%- assign n = 0 %}
-{%- for item in my_roots %}
-    {%- assign n = n | plus: 1 %}
-    {%- assign vkey = item.key | times: 1 %}
-    {%- if vkey < 10 %}
-        {%- assign my_loops = n %}
-    {%- endif -%}
-{%- endfor %}
 {%- comment -%}
 *
 *  π(10000) Schema
