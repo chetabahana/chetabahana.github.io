b80108bdf2818fec47b823993ff193f9ffd042f1
Chetabahana <chetabahana@gmail.com>
2021-03-31 00:24:20 +0700

    for i in (1..168)
    Files related to this commit are:
    _layouts/feed.rb
    
    Documentation are available in bahasa:
    https://github.com/chetabahana/chetabahana.github.io/wiki
    https://github.com/chetabahana/chetabahana.github.io/wiki/Jekyll-Liquid

--- _layouts/feed.rb
+++ _layouts/feed.rb
@@ -175,6 +175,7 @@ assets: [29,6116,3481,3747,2804,3050,3320,1233,2498,1833,1917,1777,853,830,655,1
 {%- assign π_10 = 10 | minus: 1 %}
 {%- assign my_roots = site.data.roots %}
 {%- assign my_loop = my_loop | plus: π1000 | minus: my_roots[π_10].key %}
+{%- for i in (1..168) %}{%- assign my_loops = my_loops | push: forloop.index %}{% endfor %}
 {%- comment -%}
 *
 *    Sub  | i  |   β | f   
