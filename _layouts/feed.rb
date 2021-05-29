9fa50599f06c095187a69ba12faba0f8edd19284
Chetabahana <chetabahana@gmail.com>
2021-05-29 11:22:56 +0700

    Update feed.rb

--- _layouts/feed.rb
+++ _layouts/feed.rb
@@ -23,7 +23,7 @@ assets: [29,6116,3481,3747,2804,3050,3320,1233,2498,1833,1917,1777,853,830,655,1
 *  π(9) = 4
 *  π(10) = 4
 *
-*  Reference:
+*  References:
 *  https://primes.utm.edu/howmany.html
 *  https://math.stackexchange.com/a/3545726
 * 
